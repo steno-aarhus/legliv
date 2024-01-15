f8ca3e65b7e3619d57971a2deec362125194e5a8
Niels Bock <nb@ph.au.dk>
2024-01-15 15:22:47 +0100

    Added unifnished code to time dependent variables

--- R/Main analysis.R
+++ R/Main analysis.R
@@ -9,6 +9,30 @@
 # 3. date of liver cancer diagnosis
 # 4. loss to follow-up
 
+# From time-to-event lecture notes
+# We create the time-dependent covariate (postwin) using the function tmerge.
+# The purpose of tmerge is to merge time-dependent information (covariate) to a
+# survival data set. This is done in two steps: 1) a initial setup step where a
+# data set with delayed entry variable tstart, the time variable tstop and a
+# status indicator of which we name (here status). 2) We merge the time
+# dependent information. Note that the create data set (here
+# academy_nominated_split) obtain attributes, which are used in further adding
+# time dependent covariates. Any tidy manipulation of the created data set
+# will remove these attributes.
+temp <- ukb_dataset
+# Step 1.
+ukb_dataset_split <- tmerge(temp, temp,
+                                  id=id,
+                                  status = event(right_censoring, as.numeric(liver_cancer==1)),
+                                  tstart = finished_oxford_webq,
+                                  tstop = right_censoring
+)
+# Step 2.
+ukb_dataset_split <- tmerge(ukb_dataset_split, academy_wins,
+                                  id=identity,
+                                  postwin=tdc(firstwinage))
+
+
 # step 2: substitution analysis
 # Loading package
 library(survival)
