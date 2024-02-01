fdf59024969836d8774000fedec62d524034eef1
Niels Bock <nb@ph.au.dk>
2024-02-01 14:01:45 +0000

    Created document containing relevant packages to install every time

---
+++ doc/packages.R
@@ -0,0 +1,19 @@
+install.packages('dplyr')
+install.packages('tidyverse')
+install.packages('magrittr')   # For the %$% composition pipe.
+install.packages('Hmisc')      # smean.cl.normal, smean.sdl.
+install.packages('DescTools')  # MedianCI.
+install.packages('confintr')   # ci_median, ci_sd.
+install.packages('blandr')     # blandr.output.text, blandr.statistics
+install.packages('parameters') # parameters.
+install.packages('psych')      # describe
+
+library(dplyr)
+library(tidyverse)
+library(magrittr)   # For the %$% composition pipe.
+library(Hmisc)      # smean.cl.normal, smean.sdl.
+library(DescTools)  # MedianCI.
+library(confintr)   # ci_median, ci_sd.
+library(blandr)     # blandr.output.text, blandr.statistics
+library(parameters) # parameters.
+library(psych)      # describe
