5ddbf6b39430279ad00a4f1f67ec1a90f60c5283
Niels Bock <nb@ph.au.dk>
2024-02-05 14:14:22 +0000

    mutated education categories into 1 of 3 categories. validated against Daniel's code.

--- doc/education.R
+++ doc/education.R
@@ -3,15 +3,16 @@ data_edu <- data %>%
     select(education)
 view(data_edu)
 
-# each participants have multiple categories. The following code is not correct.
+# each participants have multiple categories. Recoding to 1 of 3 categories:
 data_edu <- data_edu %>%
     mutate(education2 = case_when(
-        grepl("College", education, ignore.case = TRUE) ~ "1 College or University degree",
-        grepl("A levels/AS levels", education, ignore.case = TRUE) ~ "2 A levels/AS levels or equivalent",
-        grepl("O levels", education, ignore.case = TRUE) ~ "3 O levels/GSCEs or equivalent",
-        grepl("CSEs", education, ignore.case = TRUE) ~ "4 CSEs or equivalent",
-        grepl("NVQ or HND", education, ignore.case = TRUE) ~ "5 NVQ or HND or HNC or equivalent",
-        grepl("Other professional", education, ignore.case = TRUE) ~ "6 Other professional qualifications eg: nursing, teaching",
+        grepl("College", education, ignore.case = TRUE) ~ "high",
+        grepl("A levels/AS levels", education, ignore.case = TRUE) ~ "intermediate",
+        grepl("O levels", education, ignore.case = TRUE) ~ "intermediate",
+        grepl("CSEs", education, ignore.case = TRUE) ~ "low",
+        grepl("NVQ or HND", education, ignore.case = TRUE) ~ "low",
+        grepl("Other professional", education, ignore.case = TRUE) ~ "low",
+        grepl("None of the above", education, ignore.case = TRUE) ~ "low",
         TRUE ~ as.character(education)
     ))
 
@@ -20,22 +21,26 @@ data_edu %>%
     mutate(percentage = n / sum(n) * 100)
 
 data_edu %>%
+    summary
+
+data_edu %>%
     ggplot(aes(x = education2)) +
     geom_bar()
 
-
 # For liver data
+
 data_edu_liver <- data_liver %>%
     select(education)
 
 data_edu_liver <- data_edu_liver %>%
     mutate(education2 = case_when(
-        grepl("College", education, ignore.case = TRUE) ~ "1 College or University degree",
-        grepl("A levels/AS levels", education, ignore.case = TRUE) ~ "2 A levels/AS levels or equivalent",
-        grepl("O levels", education, ignore.case = TRUE) ~ "3 O levels/GSCEs or equivalent",
-        grepl("CSEs", education, ignore.case = TRUE) ~ "4 CSEs or equivalent",
-        grepl("NVQ or HND", education, ignore.case = TRUE) ~ "5 NVQ or HND or HNC or equivalent",
-        grepl("Other professional", education, ignore.case = TRUE) ~ "6 Other professional qualifications eg: nursing, teaching",
+        grepl("College", education, ignore.case = TRUE) ~ "high",
+        grepl("A levels/AS levels", education, ignore.case = TRUE) ~ "intermediate",
+        grepl("O levels", education, ignore.case = TRUE) ~ "intermediate",
+        grepl("CSEs", education, ignore.case = TRUE) ~ "low",
+        grepl("NVQ or HND", education, ignore.case = TRUE) ~ "low",
+        grepl("Other professional", education, ignore.case = TRUE) ~ "low",
+        grepl("None of the above", education, ignore.case = TRUE) ~ "low",
         TRUE ~ as.character(education)
     ))
 
