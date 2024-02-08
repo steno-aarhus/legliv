3794c6bdf0941694dfd08c5b2c6b3849896c3b5d
niels bock <nb@ph.au.dk>
2024-02-08 15:16:00 +0000

    added cholecystectomy. rewrote some code too

--- doc/diseases.R
+++ doc/diseases.R
@@ -13,10 +13,10 @@ data_dia <- data %>%
                str_detect(icd9, "250")) %>%
     select(icd10, icd9)
 
-# dataset contains 8066 observations, i.e, 8066 participants with at least one diabetes ICD10-code.
+# dataset contains 8100 observations, i.e, 8100 participants with at least one diabetes ICD10-code.
 # to get percentage of whole cohort:
 
-(8100/126812)*100
+nrow(data_dia) / nrow(data) * 100
 
 
 # Same for liver cancer data:
@@ -29,7 +29,7 @@ data_dia_liver <- data_liver %>%
                str_detect(icd9, "250")) %>%
     select(icd10, icd9)
 
-(40/115)*100
+nrow(data_dia_liver) / nrow(data_liver) * 100
 
 
 # For cholelithiasis
@@ -39,14 +39,15 @@ data_lith <- data %>%
                str_detect(icd9, "574")) %>%
     select(icd10, icd9)
 
-    (5880/126812)*100
+nrow(data_lith) / nrow(data) * 100
 
 data_lith_liver <- data_liver %>%
     filter(str_detect(icd10, "K80")|
                str_detect(icd9, "574")) %>%
     select(icd10, icd9)
 
-(26/115)*100
+nrow(data_lith_liver) / nrow(data_liver) * 100
+
 
 # Alcoholic liver disease
 
@@ -54,13 +55,14 @@ data_alc <- data %>%
     filter(str_detect(icd10, "K70")) %>%
     select(icd10) # no data from icd9
 
-(221/126812)*100
+nrow(data_alc) / nrow(data) * 100
 
 data_alc_liver <- data_liver %>%
     filter(str_detect(icd10, "K70")) %>%
     select(icd10)
 
-(10/115)*100
+nrow(data_alc_liver) / nrow(data_liver) * 100
+
 
 
 # NAFLD:
@@ -70,10 +72,28 @@ data_nafld <- data %>%
     filter(str_detect(icd10, "K76.0")) %>%
     select(icd10) # no data from icd9
 
-(1389/126812)*100
+nrow(data_nafld) / nrow(data) * 100
+
 
 data_nafld_liver <- data_liver %>%
     filter(str_detect(icd10, "K76.0")) %>%
     select(icd10)
 
-(14/115)*100
+nrow(data_nafld_liver) / nrow(data_liver) * 100
+
+
+# cholecystectomy
+
+data_cyst <- data %>%
+    filter(str_detect(opcs4, "J18")|
+               str_detect(opcs3, "522")) %>%
+    select(opcs4, opcs3)
+
+nrow(data_cyst) / nrow(data) * 100
+
+data_cyst_liv <- data_liver %>%
+    filter(str_detect(opcs4, "J18")|
+               str_detect(opcs3, "522")) %>%
+    select(opcs4, opcs3)
+
+nrow(data_cyst_liv) / nrow(data_liver) * 100
