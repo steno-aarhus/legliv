5beb022fcd924e6bf172dedc2d4216eaeec727a1
niels bock <nb@ph.au.dk>
2024-02-09 12:24:51 +0000

    corrected diabetes code to only contain insulin non-dependent diabetes

--- doc/diseases.R
+++ doc/diseases.R
@@ -5,12 +5,7 @@
 # (tried different filter codes, e.g. filtering for "E11.1", and compared with UKB data showcase.
 # Some values don't add up. Some participants probably have more than one diabetes ICD10-code.)
 data_dia <- data %>%
-    filter(str_detect(icd10, "E10")|
-               str_detect(icd10, "E11")|
-               str_detect(icd10, "E12")|
-               str_detect(icd10, "E13")|
-               str_detect(icd10, "E14")|
-               str_detect(icd9, "250")) %>%
+    filter(str_detect(icd10, "E11")) %>%
     select(icd10, icd9)
 
 # dataset contains 8100 observations, i.e, 8100 participants with at least one diabetes ICD10-code.
@@ -21,12 +16,7 @@ nrow(data_dia) / nrow(data) * 100
 
 # Same for liver cancer data:
 data_dia_liver <- data_liver %>%
-    filter(str_detect(icd10, "E10")|
-               str_detect(icd10, "E11")|
-               str_detect(icd10, "E12")|
-               str_detect(icd10, "E13")|
-               str_detect(icd10, "E14")|
-               str_detect(icd9, "250")) %>%
+    filter(str_detect(icd10, "E11")) %>%
     select(icd10, icd9)
 
 nrow(data_dia_liver) / nrow(data_liver) * 100
