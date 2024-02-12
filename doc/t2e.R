e1c61befb9d889522190723007cdcbdb7a8afaf3
nielsbock <nb@ph.au.dk>
2024-02-12 12:09:32 +0000

    moved some code from runfirst script

--- doc/t2e.R
+++ doc/t2e.R
@@ -1,54 +1,45 @@
-# Time to event dataset:
 
-# For cohort:
-data_t2e <- data %>%
-    select(birth_year,
-           month_of_birth,
-           dead_date,
-           age_dead,
-           dead_cause,
-           cancer_date0,
-           cancer_age0,
-           cancer0,
-           cancer_date1,
-           cancer_age1,
-           cancer1,
-           cancer_date2,
-           cancer_age2,
-           cancer2,
-           cancer_date3,
-           cancer_age3,
-           cancer3,
-           ques_comp_n,
-           ques_comp_t0,
-           ques_comp_t1,
-           ques_comp_t2,
-           ques_comp_t3,
-           ques_comp_t4)
+# Merging birth year and month of birth into one column:
 
-# For liver cancer cases:
+month_names <- c("January", "February", "March", "April", "May", "June",
+                 "July", "August", "September", "October", "November", "December")
 
-data_t2e_liver <- data_liver %>%
-    select(birth_year,
-           month_of_birth,
-           dead_date,
-           age_dead,
-           dead_cause,
-           cancer_date0,
-           cancer_age0,
-           cancer0,
-           cancer_date1,
-           cancer_age1,
-           cancer1,
-           cancer_date2,
-           cancer_age2,
-           cancer2,
-           cancer_date3,
-           cancer_age3,
-           cancer3,
-           ques_comp_n,
-           ques_comp_t0,
-           ques_comp_t1,
-           ques_comp_t2,
-           ques_comp_t3,
-           ques_comp_t4)
+data <- data %>%
+    mutate(month_of_birth_num = sprintf("%02d", match(month_of_birth, month_names)))
+
+data <- data %>%
+    unite(birth, birth_year, month_of_birth_num, sep = "-")
+
+remove(month_names)
+
+data <- data %>%
+    select(-month_of_birth)
+
+# adding 15 as DD for all participants:
+
+data$birth <- as.Date(paste0(data$birth, "-15"))
+
+# Removing specific time stamp from date of completed questionnaires:
+# (May be irrelevant)
+
+data <- data %>%
+    mutate(ques_comp_t0 = substr(ques_comp_t0, 1, 10),
+           ques_comp_t1 = substr(ques_comp_t1, 1, 10),
+           ques_comp_t2 = substr(ques_comp_t2, 1, 10),
+           ques_comp_t3 = substr(ques_comp_t3, 1, 10),
+           ques_comp_t4 = substr(ques_comp_t4, 1, 10)
+    )
+
+# Creating baseline age:
+
+# Creating age at baseline:
+# (Takes a long time to load)
+data <- data %>%
+   mutate(age_at_baseline = year(baseline_start_date) - year(birth) -
+             ifelse(month(baseline_start_date) < month(birth) |
+                       (month(baseline_start_date) == month(birth) &
+                           day(baseline_start_date) < day(birth)), 1, 0))
+
+
+
+# Go back to runfirst to create liver cancer event cohort
