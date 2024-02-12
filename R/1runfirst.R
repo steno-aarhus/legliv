a0059e5e922e539bc18311b9120b1c80f8837869
niels bock <nb@ph.au.dk>
2024-02-12 15:40:13 +0000

    fixed id column issue. added back time to event code

--- R/1runfirst.R
+++ R/1runfirst.R
@@ -61,6 +61,38 @@ data <- data %>%
 data <- data %>%
     select(where(~ !all(is.na(.))))
 
+# Merging birth year and month of birth into one column:
+
+month_names <- c("January", "February", "March", "April", "May", "June",
+                 "July", "August", "September", "October", "November", "December")
+
+data <- data %>%
+  mutate(month_of_birth_num = sprintf("%02d", match(month_of_birth, month_names)))
+
+data <- data %>%
+  unite(birth, birth_year, month_of_birth_num, sep = "-")
+
+remove(month_names)
+
+data <- data %>%
+  select(-month_of_birth)
+
+# adding 15 as DD for all participants:
+
+data$birth <- as.Date(paste0(data$birth, "-15"))
+
+# Removing specific time stamp from date of completed questionnaires:
+# (May be irrelevant)
+
+data <- data %>%
+  mutate(ques_comp_t0 = substr(ques_comp_t0, 1, 10),
+         ques_comp_t1 = substr(ques_comp_t1, 1, 10),
+         ques_comp_t2 = substr(ques_comp_t2, 1, 10),
+         ques_comp_t3 = substr(ques_comp_t3, 1, 10),
+         ques_comp_t4 = substr(ques_comp_t4, 1, 10)
+  )
+
+
   # creating id column:
 data <- data %>%
   mutate(id = 1:nrow(data))
@@ -98,7 +130,17 @@ data <- data %>%
   filter(is.na(lead(completion_date))) %>%
   # Rename the columns to match the desired output
   rename(baseline_start_date = completion_date) %>%
-  select(-starts_with("ques_comp_t"))
+  ungroup()
+
+# Creating baseline age:
+
+# Creating age at baseline:
+# (Takes a long time to load)
+data <- data %>%
+  mutate(age_at_baseline = year(baseline_start_date) - year(birth) -
+           ifelse(month(baseline_start_date) < month(birth) |
+                    (month(baseline_start_date) == month(birth) &
+                       day(baseline_start_date) < day(birth)), 1, 0))
 
 # Removing all participants who have had liver cancer before baseline :
 
