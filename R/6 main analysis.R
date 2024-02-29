00e4f11847dc72ffbd896a3469139b0828216848
niels bock <nb@ph.au.dk>
2024-02-29 20:45:53 +0000

    changed food variables to 25 grams in models. Tried splining.

--- R/6 main analysis.R
+++ R/6 main analysis.R
@@ -1,72 +1,66 @@
 library(survival)
 install.packages('parameters')
 library(parameters)
+install.packages('rms')
+library(rms)
 
 # Model 1:
 # minimally adjusted for age at recruitment, sex, total energy intake, and all other dietary components:
 model1 <- coxph(Surv(time = time, event = status=="liver cancer") ~
-                     legume_daily +
-                     red_proc_meat_daily +
+                     legume_daily_25 +
+                     red_proc_meat_daily_25 +
                      age_at_baseline +
                      sex +
                      total_energy_food_daily +
-                     poultry_daily +
-                     fish_daily +
-                     dairy_daily +
-                     egg_daily +
-                     cereal_refined_daily +
-                     whole_grain_daily +
-                     veggie_daily +
-                     potato_daily +
-                     fruit_daily +
-                     nut_daily +
-                     meat_sub_daily +
-                     snack_daily +
-                     mixed_dish_daily +
-                     sauce_daily +
-                     fats_daily +
-                     non_alc_beverage_daily +
-                     alc_beverage_daily,
+                     poultry_daily_25 +
+                     fish_daily_25 +
+                     dairy_daily_25 +
+                     egg_daily_25 +
+                     cereal_refined_daily_25 +
+                     whole_grain_daily_25 +
+                     veggie_daily_25 +
+                     potato_daily_25 +
+                     fruit_daily_25 +
+                     nut_daily_25 +
+                     meat_sub_daily_25 +
+                     snack_daily25 +
+                     mixed_dish_daily_25 +
+                     sauce_daily_25 +
+                     fats_daily_25 +
+                     non_alc_beverage_daily_25 +
+                     alc_beverage_daily_25,
                  data = data)
 
 model1 %>%
     parameters(exponentiate = T)
-
-coefficients <- coef(model1)
-
-# Summarize the coefficients
-hazard_ratio <- exp(sum(coefficients[-1]))  # Exclude the intercept term
-
-# Print the hazard ratio
-print(hazard_ratio)
 
 # Model 2:
 # Further adjusted for education, TDI, ethnicity, BMI, physical activity, smoking, alcohol,
 # waist circumference, T2D, cholelithiasis, and cholecystectomy
 
 model2 <- coxph(Surv(time = time, event = status=="liver cancer") ~
-                    legume_daily +
-                    red_proc_meat_daily +
+                    legume_daily_25 +
+                    red_proc_meat_daily_25 +
                     age_at_baseline +
                     sex +
                     total_energy_food_daily +
-                    poultry_daily +
-                    fish_daily +
-                    dairy_daily +
-                    egg_daily +
-                    cereal_refined_daily +
-                    whole_grain_daily +
-                    veggie_daily +
-                    potato_daily +
-                    fruit_daily +
-                    nut_daily +
-                    meat_sub_daily +
-                    snack_daily +
-                    mixed_dish_daily +
-                    sauce_daily +
-                    fats_daily +
-                    non_alc_beverage_daily +
-                    alc_beverage_daily +
+                    poultry_daily_25 +
+                    fish_daily_25 +
+                    dairy_daily_25 +
+                    egg_daily_25 +
+                    cereal_refined_daily_25 +
+                    whole_grain_daily_25 +
+                    veggie_daily_25 +
+                    potato_daily_25 +
+                    fruit_daily_25 +
+                    nut_daily_25 +
+                    meat_sub_daily_25 +
+                    snack_daily25 +
+                    mixed_dish_daily_25 +
+                    sauce_daily_25 +
+                    fats_daily_25 +
+                    non_alc_beverage_daily_25 +
+                    alc_beverage_daily_25 +
                     education +
                     tdi +
                     ethnicity +
@@ -81,25 +75,33 @@ model2 %>%
 
 
 
-model0 <- coxph(Surv(time = time, event = status=='liver cancer') ~ red_proc_meat_daily,
+model0 <- coxph(Surv(time = time, event = status=='liver cancer') ~ legume_daily_25 + red_proc_meat_daily_25 + total_energy_food_daily,
                 data = data)
 
 model0 %>%
-    summary()
+    parameters(exponentiate = T)
 
-data %>%
-    select(time) %>%
-    summary()
 
-data %>%
-    select(total_energy_food_daily) %>%
-    summary()
+fit <- coxph(Surv(time = time, event = status=="liver cancer") ~ rcs(legume_daily, 4),
+             data = data, ties='breslow')
 
-data_liver %>%
-    ggplot(aes(x = total_energy_food_daily)) +
-    geom_histogram(bins = 15)
+# Generate values of red_proc_meat_daily to cover the range of your data
+legume_daily_values <- seq(min(data$legume_daily), 50, length.out = 100)
 
-data_liver %>%
-    ggplot(aes(sample = total_energy_food_daily)) +
-    stat_qq() +
-    stat_qq_line()
+# Predict the hazard ratios and their standard errors using the fitted model
+predictions <- predict(fit, newdata = data.frame(legume_daily = legume_daily_values), type = "terms", se.fit = TRUE)
+
+# Extract predicted hazard ratios and standard errors
+predicted_hr <- exp(predictions$fit)
+se_hr <- predictions$se.fit
+
+# Calculate upper and lower bounds of the confidence interval
+upper_bound <- exp(log(predicted_hr) + 1.96 * se_hr)
+lower_bound <- exp(log(predicted_hr) - 1.96 * se_hr)
+
+# Plot the predicted hazard ratios against legume_daily
+plot(legume_daily_values, predicted_hr, type = "l", xlab = "legume_daily", ylab = "Hazard Ratio", main = "Spline Function of legume_daily", ylim = c(0.5, 1.5))
+
+# Add confidence interval
+lines(legume_daily_values, upper_bound, col = "blue", lty = 2)
+lines(legume_daily_values, lower_bound, col = "blue", lty = 2)
