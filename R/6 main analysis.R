8b22efbb85ddb48e5ef249f18d3551da3e954d72
niels bock <nb@ph.au.dk>
2024-03-04 14:45:51 +0000

    added last disease covariables. added models for red and processed meat separately

--- R/6 main analysis.R
+++ R/6 main analysis.R
@@ -4,64 +4,107 @@ library(parameters)
 install.packages('rms')
 library(rms)
 
-# Model 1:
+# Model 1: leave one out (red_proc_meat_daily)
 # minimally adjusted for age at recruitment, sex, total energy intake, and all other dietary components:
 model1 <- coxph(Surv(time = status_age, event = status=="liver cancer") ~
-                     legume_daily_25 +
-                     red_proc_meat_daily_25 +
-                     age_at_baseline +
-                     sex +
-                     poultry_daily +
-                     fish_daily +
-                     dairy_daily +
-                     egg_daily +
-                     cereal_refined_daily +
-                     whole_grain_daily +
-                     veggie_daily +
-                     potato_daily +
-                     fruit_daily +
-                     nut_daily +
-                     meat_sub_daily +
-                     snack_daily +
-                     mixed_dish_daily +
-                     sauce_daily +
-                     fats_daily +
-                     non_alc_beverage_daily +
-                     alc_beverage_daily,
-                 data = data)
+                    legume_daily_25 +
+                    sex +
+                    age_at_baseline +
+                    total_weight_food_daily +
+                    poultry_daily +
+                    fish_daily +
+                    dairy_daily +
+                    egg_daily +
+                    cereal_refined_daily +
+                    whole_grain_daily +
+                    veggie_daily +
+                    potato_daily +
+                    fruit_daily +
+                    nut_daily +
+                    meat_sub_daily +
+                    snack_daily +
+                    mixed_dish_daily +
+                    sauce_daily +
+                    fats_daily +
+                    non_alc_beverage_daily +
+                    alc_beverage_daily,
+                data = data)
 
 model1 %>%
     parameters(exponentiate = T)
 
-difhr <- exp(model1$coefficients[2]-model1$coefficients[1])
-difhr
+# subtraction model
+# difhr <- exp(model1$coefficients[1]-model1$coefficients[2])
+# difhr
 
-data <- data %>%
-    mutate(weight = legume_daily +
-               red_proc_meat_daily +
-               poultry_daily +
-               fish_daily +
-               dairy_daily +
-               egg_daily +
-               cereal_refined_daily +
-               whole_grain_daily +
-               veggie_daily +
-               potato_daily +
-               fruit_daily +
-               nut_daily +
-               meat_sub_daily +
-               snack_daily +
-               mixed_dish_daily +
-               sauce_daily +
-               fats_daily +
-               non_alc_beverage_daily +
-               alc_beverage_daily)
 
-model1_1 <- coxph(Surv(time = status_age, event = status=="liver cancer") ~
+# Model 1 but only red meat:
+model1_red <- coxph(Surv(time = status_age, event = status=="liver cancer") ~
                     legume_daily_25 +
-                      weight +
-                    age_at_baseline +
+                    proc_meat_daily +
                     sex +
+                    age_at_baseline +
+                    total_weight_food_daily +
+                    poultry_daily +
+                    fish_daily +
+                    dairy_daily +
+                    egg_daily +
+                    cereal_refined_daily +
+                    whole_grain_daily +
+                    veggie_daily +
+                    potato_daily +
+                    fruit_daily +
+                    nut_daily +
+                    meat_sub_daily +
+                    snack_daily +
+                    mixed_dish_daily +
+                    sauce_daily +
+                    fats_daily +
+                    non_alc_beverage_daily +
+                    alc_beverage_daily,
+                data = data)
+
+model1_red %>%
+    parameters(exponentiate = T)
+
+# Model 1 but only processed meat:
+model1_proc <- coxph(Surv(time = status_age, event = status=="liver cancer") ~
+                        legume_daily_25 +
+                        red_meat_daily +
+                        sex +
+                        age_at_baseline +
+                        total_weight_food_daily +
+                        poultry_daily +
+                        fish_daily +
+                        dairy_daily +
+                        egg_daily +
+                        cereal_refined_daily +
+                        whole_grain_daily +
+                        veggie_daily +
+                        potato_daily +
+                        fruit_daily +
+                        nut_daily +
+                        meat_sub_daily +
+                        snack_daily +
+                        mixed_dish_daily +
+                        sauce_daily +
+                        fats_daily +
+                        non_alc_beverage_daily +
+                        alc_beverage_daily,
+                    data = data)
+
+model1_proc %>%
+    parameters(exponentiate = T)
+
+# Model 2:
+# Further adjusted for education, TDI, ethnicity, BMI, physical activity, smoking, alcohol,
+# waist circumference, T2D, cholelithiasis, and cholecystectomy
+
+model2 <- coxph(Surv(time = status_age, event = status=="liver cancer") ~
+                    legume_daily_25 +
+                    sex +
+                    age_at_baseline +
+                    total_weight_food_daily +
                     poultry_daily +
                     fish_daily +
                     dairy_daily +
@@ -79,66 +122,164 @@ model1_1 <- coxph(Surv(time = status_age, event = status=="liver cancer") ~
                     fats_daily +
                     non_alc_beverage_daily +
                     alc_beverage_daily +
-                      education +
-                      bmi +
-                      wc +
-                      tdi +
-                      smoking +
-                      phys_acti,
+                    education +
+                    tdi +
+                    ethnicity +
+                    bmi_category +
+                    exercise +
+                    smoking +
+                    wc +
+                    diabetes +
+                    cholelith +
+                    alc_liver +
+                    nafld +
+                    cystectomy,
                 data = data)
 
-model1_1 %>%
+model2 %>%
     parameters(exponentiate = T)
 
-# Model 2:
-# Further adjusted for education, TDI, ethnicity, BMI, physical activity, smoking, alcohol,
-# waist circumference, T2D, cholelithiasis, and cholecystectomy
-
-model2 <- coxph(Surv(time = status_age, event = status=="liver cancer") ~
+# model 2 but only red meat
+model2_red <- coxph(Surv(time = status_age, event = status=="liver cancer") ~
                     legume_daily_25 +
-                    red_proc_meat_daily_25 +
-                    age_at_baseline +
+                    proc_meat_daily +
                     sex +
-                    poultry_daily_25 +
-                    fish_daily_25 +
-                    dairy_daily_25 +
-                    egg_daily_25 +
-                    cereal_refined_daily_25 +
-                    whole_grain_daily_25 +
-                    veggie_daily_25 +
-                    potato_daily_25 +
-                    fruit_daily_25 +
-                    nut_daily_25 +
-                    meat_sub_daily_25 +
-                    snack_daily25 +
-                    mixed_dish_daily_25 +
-                    sauce_daily_25 +
-                    fats_daily_25 +
-                    non_alc_beverage_daily_25 +
-                    alc_beverage_daily_25 +
+                    age_at_baseline +
+                    total_weight_food_daily +
+                    poultry_daily +
+                    fish_daily +
+                    dairy_daily +
+                    egg_daily +
+                    cereal_refined_daily +
+                    whole_grain_daily +
+                    veggie_daily +
+                    potato_daily +
+                    fruit_daily +
+                    nut_daily +
+                    meat_sub_daily +
+                    snack_daily +
+                    mixed_dish_daily +
+                    sauce_daily +
+                    fats_daily +
+                    non_alc_beverage_daily +
+                    alc_beverage_daily +
                     education +
                     tdi +
                     ethnicity +
                     bmi_category +
                     phys_acti +
                     smoking +
-                    wc,
+                    wc +
+                    diabetes +
+                    cholelith +
+                    alc_liver +
+                    nafld +
+                    cystectomy,
                 data = data)
 
-model2 %>%
+model2_red %>%
     parameters(exponentiate = T)
 
 
+# model 2 but only processed meat
+model2_proc <- coxph(Surv(time = status_age, event = status=="liver cancer") ~
+                        legume_daily_25 +
+                        red_meat_daily +
+                        sex +
+                        age_at_baseline +
+                        total_weight_food_daily +
+                        poultry_daily +
+                        fish_daily +
+                        dairy_daily +
+                        egg_daily +
+                        cereal_refined_daily +
+                        whole_grain_daily +
+                        veggie_daily +
+                        potato_daily +
+                        fruit_daily +
+                        nut_daily +
+                        meat_sub_daily +
+                        snack_daily +
+                        mixed_dish_daily +
+                        sauce_daily +
+                        fats_daily +
+                        non_alc_beverage_daily +
+                        alc_beverage_daily +
+                        education +
+                        tdi +
+                        ethnicity +
+                        bmi_category +
+                        phys_acti +
+                        smoking +
+                        wc +
+                        cholelith +
+                        alc_liver +
+                        nafld +
+                        cystectomy,
+                    data = data)
 
-model0 <- coxph(Surv(time = time, event = status=='liver cancer') ~ legume_daily_25 + red_proc_meat_daily_25 + total_energy_food_daily,
+model2_proc %>%
+    parameters(exponentiate = T)
+
+
+model0 <- coxph(Surv(time = time, event = status=='liver cancer') ~ proc_meat_daily_25,
                 data = data)
 
 model0 %>%
     parameters(exponentiate = T)
 
+difhr <- exp(model0$coefficients[2]-model0$coefficients[1])
+difhr
+
+
+data %>%
+    select(ends_with("daily")) %>%
+    summary()
+
+model_pulse <- coxph(Surv(time = status_age, event = status=="liver cancer") ~
+                    pulse_daily_25 +
+                    sex +
+                    age_at_baseline +
+                    total_weight_food_daily +
+                    legume_other_daily +
+                    poultry_daily +
+                    fish_daily +
+                    dairy_daily +
+                    egg_daily +
+                    cereal_refined_daily +
+                    whole_grain_daily +
+                    veggie_daily +
+                    potato_daily +
+                    fruit_daily +
+                    nut_daily +
+                    meat_sub_daily +
+                    snack_daily +
+                    mixed_dish_daily +
+                    sauce_daily +
+                    fats_daily +
+                    non_alc_beverage_daily +
+                    alc_beverage_daily +
+                        education +
+                        tdi +
+                        ethnicity +
+                        bmi_category +
+                        phys_acti +
+                        smoking +
+                        wc +
+                        cholelith +
+                        alc_liver +
+                        nafld +
+                        cystectomy,
+                data = data)
+
+model_pulse %>%
+    parameters(exponentiate = T)
+
 
 fit <- coxph(Surv(time = time, event = status=="liver cancer") ~ rcs(legume_daily, 4),
              data = data, ties='breslow')
+fit %>%
+    parameters(exponentiate = T)
 
 # Generate values of red_proc_meat_daily to cover the range of your data
 legume_daily_values <- seq(min(data$legume_daily), 50, length.out = 100)
