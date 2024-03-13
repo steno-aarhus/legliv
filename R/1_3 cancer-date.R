2198f56dd657ad86987f06361a04c584916ea2ce
niels bock <nb@ph.au.dk>
2024-03-13 14:33:39 +0000

    minor edits

--- R/1_3 cancer-date.R
+++ R/1_3 cancer-date.R
@@ -16,3 +16,22 @@ data_long <- data_long %>%
 # Convert back to wide format
 data_wide <- data_long %>%
     pivot_wider(names_from = date_index, values_from = cancer_date)
+
+data_date <- data %>%
+    left_join(data_wide %>% select(id, cancer_date1), by = "id")
+
+data_date1 <- data_date %>%
+    select(cancer_date1, starts_with("p40005"))
+
+# Fie kode
+icd10_subset <- data %>%
+    select(matches("p41270|p41280|id")) %>%
+    pivot_longer(cols = matches("_a[0-9]*$"),
+                 names_to = c(".value", "a"),
+                 names_sep = "_")
+
+icd10_hcc <- icd10_subset %>%
+    mutate(icd10_hcc_date = ifelse(str_detect(p41270var, "C22.0"),
+                                     as.character(c_across(starts_with("p41280"))),
+                                     NA),
+           icd10_hcc_date = as.Date(icd10_hcc_date, format = "%Y-%m-%d"))
