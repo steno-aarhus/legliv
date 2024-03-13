27aaf08636c81ff9928c6f0710dfda078fe70ca4
niels bock <nb@ph.au.dk>
2024-03-13 16:12:49 +0000

    new script to detect cancer events

--- R/1_3 cancer-date.R
+++ R/1_3 cancer-date.R
@@ -1,28 +1,3 @@
-# Convert to long format
-data_long <- data %>%
-    select(starts_with("p40006"),starts_with("p40005"), id) %>%
-    pivot_longer(cols = starts_with("p40006"), names_to = "cancer_index", values_to = "cancer_diagnosis") %>%
-    pivot_longer(cols = starts_with("p40005"), names_to = "date_index", values_to = "cancer_date") %>%
-    mutate(cancer_index = parse_number(cancer_index),
-           date_index = parse_number(date_index)) %>%
-    filter(cancer_diagnosis == "C22.0 Liver cell carcinoma" |
-               cancer_diagnosis == "C22.1 Intrahepatic bile duct carcinoma") %>%
-    select(-cancer_diagnosis)
-
-# Extract the corresponding cancer diagnosis dates
-data_long <- data_long %>%
-    mutate(cancer_date1 = lead(cancer_date, order_by = cancer_index))
-
-# Convert back to wide format
-data_wide <- data_long %>%
-    pivot_wider(names_from = date_index, values_from = cancer_date)
-
-data_date <- data %>%
-    left_join(data_wide %>% select(id, cancer_date1), by = "id")
-
-data_date1 <- data_date %>%
-    select(cancer_date1, starts_with("p40005"))
-
 # Fie kode
 icd10_subset <- data %>%
     select(matches("p41270|p41280|id")) %>%
@@ -32,6 +7,67 @@ icd10_subset <- data %>%
 
 icd10_hcc <- icd10_subset %>%
     mutate(icd10_hcc_date = ifelse(str_detect(p41270var, "C22.0"),
-                                     as.character(c_across(starts_with("p41280"))),
-                                     NA),
+                                   as.character(c_across(starts_with("p41280"))),
+                                   NA),
            icd10_hcc_date = as.Date(icd10_hcc_date, format = "%Y-%m-%d"))
+
+first_non_na_hcc <- icd10_hcc %>%
+    filter(!is.na(icd10_hcc_date)) %>%
+    group_by(id) %>%
+    slice(1) %>%
+    ungroup()
+
+data <- data %>%
+    left_join(first_non_na_hcc %>% select(id, icd10_hcc_date), by = "id")
+
+icd10_icc <- icd10_subset %>%
+    mutate(icd10_icc_date = ifelse(str_detect(p41270var, "C22.1"),
+                                   as.character(c_across(starts_with("p41280"))),
+                                   NA),
+           icd10_icc_date = as.Date(icd10_icc_date, format = "%Y-%m-%d"))
+
+first_non_na_icc <- icd10_icc %>%
+    filter(!is.na(icd10_icc_date)) %>%
+    group_by(id) %>%
+    slice(1) %>%
+    ungroup()
+
+data <- data %>%
+    left_join(first_non_na_icc %>% select(id, icd10_icc_date), by = "id")
+
+
+cancer_subset <- data %>%
+    select(matches("p40006|p40005|id")) %>%
+    pivot_longer(cols = matches("_i[0-9]*$"),
+                 names_to = c(".value", "a"),
+                 names_sep = "_")
+
+cancer_hcc <- cancer_subset %>%
+    mutate(cancer_hcc_date = ifelse(str_detect(p40006, "C22.0"),
+                                    as.character(c_across(starts_with("p40005"))),
+                                    NA),
+           cancer_hcc_date = as.Date(cancer_hcc_date, format = "%Y-%m-%d"))
+
+first_non_na_hcc_c <- cancer_hcc %>%
+    filter(!is.na(cancer_hcc_date)) %>%
+    group_by(id) %>%
+    slice(1) %>%
+    ungroup()
+
+data <- data %>%
+    left_join(first_non_na_hcc_c %>% select(id, cancer_hcc_date), by = "id")
+
+cancer_icc <- cancer_subset %>%
+    mutate(cancer_icc_date = ifelse(str_detect(p40006, "C22.1"),
+                                    as.character(c_across(starts_with("p40005"))),
+                                    NA),
+           cancer_icc_date = as.Date(cancer_icc_date, format = "%Y-%m-%d"))
+
+first_non_na_icc_c <- cancer_icc %>%
+    filter(!is.na(cancer_icc_date)) %>%
+    group_by(id) %>%
+    slice(1) %>%
+    ungroup()
+
+data <- data %>%
+    left_join(first_non_na_icc_c %>% select(id, cancer_icc_date), by = "id")
