4e8545eaa644856d8e1583a36eb10ffc9c42a611
niels bock <nb@ph.au.dk>
2024-03-18 09:18:27 +0000

    added code to determine typical diet of completed questionnaires. added code to exclude participants who had any cancer before baseline. added code to exclude participants with high alcohol consumption.

--- R/1-data-clean-main-analysis.R
+++ R/1-data-clean-main-analysis.R
@@ -389,6 +389,11 @@ other_variables <- function(data) {
       dead_cause = p40001_i0,
       age_dead = p40007_i0
     )
+  data <- data %>%
+    mutate_at(vars(starts_with("p100020_i")), ~coalesce(., "Yes")) %>%
+    mutate(
+      typical_diet = if_else(p100020_i0 == "No" | p100020_i1 == "No" | p100020_i2 == "No" | p100020_i3 == "No"| p100020_i4 == "No", "No", "Yes")
+    )
   return(data)
 }
 data <- other_variables(data)
@@ -621,29 +626,31 @@ data <- remove_liver_before(data)
 stratify_prepare1 <- function(data) {
   data_wc_high <- data %>%
     filter(sex == "Male" & wc >= 102 | sex == "Female" & wc >= 88)
-
   data_wc_low <- data %>%
     filter(sex == "Male" & wc < 102 | sex == "Female" & wc < 88)
-
   data_diabetes_yes <- data %>%
     filter(diabetes == "Yes")
-
   data_diabetes_no <- data %>%
     filter(diabetes == "No")
-
   data_men <- data %>%
     filter(sex == "Male")
-
   data_women <- data %>%
     filter(sex == "Female")
-
+  data_alcohol <- data %>%
+    filter(
+      sex == "Male" & alcohol_daily < 30 |sex == "Female" & alcohol_daily < 20
+    )
+  data_typical_diet <- data %>%
+    filter(typical_diet == "Yes")
   return(list(
     data_wc_high = data_wc_high,
     data_wc_low = data_wc_low,
     data_diabetes_yes = data_diabetes_yes,
     data_diabetes_no = data_diabetes_no,
     data_men = data_men,
-    data_women = data_women
+    data_women = data_women,
+    data_alcohol = data_alcohol,
+    data_typical_diet = data_typical_diet
   ))
 }
 data_list <- stratify_prepare1(data)
@@ -653,6 +660,8 @@ data_diabetes_yes <- data_list$data_diabetes_yes
 data_diabetes_no <- data_list$data_diabetes_no
 data_men <- data_list$data_men
 data_women <- data_list$data_women
+data_alcohol <- data_list$data_alcohol
+data_typical_diet <-  data_list$data_typical_diet
 
 stratify_prepare2 <- function(data) {
   data_hcc <- data %>%
@@ -732,6 +741,47 @@ ques_comp_list <- filter_ques_comp_n(data)
 data_ques_3 <- ques_comp_list$data_ques_3
 data_ques_4 <- ques_comp_list$data_ques_4
 data_ques_5 <- ques_comp_list$data_ques_5
+
+cancer_register <- function(data) {
+  data_cancer_before <- data %>%
+    select(starts_with("p40006"), starts_with("p40005"), baseline_start_date, id) %>%
+    mutate(
+      cancer_before = if_else(
+        rowSums(across(starts_with("p40006_i"), ~ grepl("C\\d{2}", .x)) &
+                  across(starts_with("p40005_i"), ~ .x < baseline_start_date)) > 0,
+        "Yes",
+        "No"
+      )
+    )
+  data <- data %>%
+    left_join(data_cancer_before %>% select(id, cancer_before), by = "id")
+  return(data)
+}
+data <- cancer_register(data)
+
+cancer_icd10_register <- function(data) {
+  data_cancer_icd10_before <- data %>%
+    select(starts_with("p41270"), starts_with("p41280"), baseline_start_date, id) %>%
+    mutate(
+      cancer_icd10_before = if_else(
+        rowSums(across(starts_with("p41270var_a"), ~ grepl("C\\d{2}", .x)) &
+                  across(starts_with("p41280_a"), ~ .x < baseline_start_date)) > 0,
+        "Yes",
+        "No"
+      )
+    )
+  data <- data %>%
+    left_join(data_cancer_icd10_before %>% select(id, cancer_icd10_before), by = "id")
+  return(data)
+}
+data <- cancer_icd10_register(data)
+
+data <- data %>%
+  mutate(cancer_before_baseline = if_else(cancer_before == "No" & cancer_icd10_before == "No", "No", "Yes"))
+data %>% group_by(cancer_before_baseline) %>% summarise(n=n())
+
+data_no_cancer <- data %>%
+  filter(cancer_before_baseline == "No")
 
 data_liver <- data %>%
   filter(status == "Liver cancer")
