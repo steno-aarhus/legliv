7e820ec10cf7c018b44f3b69920499887e4bb4e6
niels bock <nb@ph.au.dk>
2024-03-18 15:07:12 +0000

    minor edit

--- R/1-data-clean-main-analysis.R
+++ R/1-data-clean-main-analysis.R
@@ -807,5 +807,5 @@ remove_liver_before <- function(data) {
 }
 data <- remove_liver_before(data)
 
-data_liver <- data %>%
-  filter(status == "Liver cancer")
+stratify_prepare1 <- function(data) {
+  data_wc_high <- data %>%
