e4943117d02429200671a6647b2c2cd8856a1f9b
niels bock <nb@ph.au.dk>
2024-03-19 15:23:43 +0000

    minor edits

--- R/9-table-legume-quintile.R
+++ R/9-table-legume-quintile.R
@@ -1,22 +1,3 @@
-model0t <- coxph(
-    Surv(time = status_age, event = status == "Liver cancer") ~
-        legume_category + red_meat_daily + proc_meat_daily +
-        whole_grain_daily + poultry_daily + fish_daily + dairy_daily +
-        egg_daily + cereal_refined_daily + veggie_daily + potato_daily +
-        fruit_daily + nut_daily + meat_sub_daily + snack_daily +
-        mixed_dish_daily + sauce_daily + fats_daily +
-        non_alc_beverage_daily + alc_beverage_daily + total_weight_food_daily,
-    data = data
-)
-
-m0t <- model0t %>%
-    tbl_regression(
-        exponentiate = T,
-        include = legume_category,
-        label = legume_category ~ "Legume category",
-    ) %>%
-    modify_caption("**Full cohort** (N = {N})")
-
 model1t <- coxph(
     Surv(time = status_age, event = status == "Liver cancer") ~
         legume_category + red_meat_daily + proc_meat_daily +
@@ -25,7 +6,7 @@ model1t <- coxph(
         fruit_daily + nut_daily + meat_sub_daily + snack_daily +
         mixed_dish_daily + sauce_daily + fats_daily +
         non_alc_beverage_daily + alc_beverage_daily + total_weight_food_daily +
-        sex + age_at_baseline,
+        sex,
     data = data
 )
 
@@ -46,7 +27,7 @@ model2t <- coxph(
         fruit_daily + nut_daily + meat_sub_daily + snack_daily +
         mixed_dish_daily + sauce_daily + fats_daily +
         non_alc_beverage_daily + alc_beverage_daily + total_weight_food_daily +
-        sex + age_at_baseline +
+        sex +
         education + tdi + ethnicity + spouse +
         bmi_category + exercise + smoking + wc + alcohol_daily +
         diabetes + cholelith + nafld + cystectomy,
@@ -61,8 +42,25 @@ m2t <- model2t %>%
     ) %>%
     modify_caption("**Full cohort** (N = {N})")
 
+data_legume_quintile <- data %>%
+    select(legume_category, legume_daily)
+
+mean_legume <- data_legume_quintile %>%
+    group_by(legume_category) %>%
+    summarise(mean_legume_daily = mean(legume_daily, na.rm = TRUE))
+
+tbl_mean_legume <- mean_legume %>%
+    tbl_summary()
+tbl_category <- data_legume_quintile %>%
+    select(-legume_daily) %>%
+    tbl_summary()
+tbl_mean_legume
+tbl_category
+tbl_merge_sum <- tbl_merge(
+    list(tbl_category, tbl_mean_legume)
+)
+tbl_merge_sum
 tbl_merge <- tbl_merge(
-    tbls = list(m0t,m1t,m2t),
-    tab_spanner = c("**Model 0**", "**Model 1**", "**Model 2**")
+    tbls = list(tbl_category, tbl_mean_legume, m1t, m2t)
 )
 tbl_merge
