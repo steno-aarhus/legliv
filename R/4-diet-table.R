a92cb25b48890ecbec666680622b662d21059cc4
niels bock <nb@ph.au.dk>
2024-03-21 15:03:28 +0000

    edited new food groups

--- R/4-diet-table.R
+++ R/4-diet-table.R
@@ -1,10 +1,10 @@
-
+library(gtsummary)
 diet_all <- data %>%
   select(
     total_energy_food_daily, total_weight_food_daily, legume_daily, total_meat_daily,
     red_meat_daily, proc_meat_daily, poultry_daily, fish_daily, dairy_daily, egg_daily,
-    cereal_refined_daily, whole_grain_daily, veggie_daily, potato_daily, fruit_daily,
-    nut_daily, meat_sub_daily, snack_daily, mixed_dish_daily, sauce_daily, fats_daily,
+    cereal_refined_daily, whole_grain_daily, veggie_daily, fruit_daily,
+    nut_daily, snack_daily, mixed_dish_daily, fats_daily,
     non_alc_beverage_daily, alc_beverage_daily
   )
 
@@ -13,8 +13,8 @@ diet_liver <- data %>%
   select(
     total_energy_food_daily, total_weight_food_daily, legume_daily, total_meat_daily,
     red_meat_daily, proc_meat_daily, poultry_daily, fish_daily, dairy_daily, egg_daily,
-    cereal_refined_daily, whole_grain_daily, veggie_daily, potato_daily, fruit_daily,
-    nut_daily, meat_sub_daily, snack_daily, mixed_dish_daily, sauce_daily, fats_daily,
+    cereal_refined_daily, whole_grain_daily, veggie_daily, fruit_daily,
+    nut_daily, snack_daily, mixed_dish_daily, fats_daily,
     non_alc_beverage_daily, alc_beverage_daily
   )
 
@@ -35,13 +35,10 @@ diet_all_table <- diet_all %>%
       cereal_refined_daily ~ "Cereals, refined",
       whole_grain_daily ~ "Cereals, whole grains",
       veggie_daily ~ "Vegetables",
-      potato_daily ~ "Potatoes",
       fruit_daily ~ "Fruit",
       nut_daily ~ "Nuts & seeds",
-      meat_sub_daily ~ "Meat substitutes, non-soy",
       snack_daily ~ "Snacks",
       mixed_dish_daily ~ "Mixed dishes",
-      sauce_daily ~ "Sauces and condiments",
       fats_daily ~ "Fat & spreads",
       non_alc_beverage_daily ~ "Beverages, non-alcoholic",
       alc_beverage_daily ~ "Beverages, alcoholic"
@@ -65,13 +62,10 @@ diet_liver_table <- diet_liver %>%
       cereal_refined_daily ~ "Cereals, refined",
       whole_grain_daily ~ "Cereals, whole grains",
       veggie_daily ~ "Vegetables",
-      potato_daily ~ "Potatoes",
       fruit_daily ~ "Fruit",
       nut_daily ~ "Nuts & seeds",
-      meat_sub_daily ~ "Meat substitutes, non-soy",
       snack_daily ~ "Snacks",
       mixed_dish_daily ~ "Mixed dishes",
-      sauce_daily ~ "Sauces and condiments",
       fats_daily ~ "Fat & spreads",
       non_alc_beverage_daily ~ "Beverages, non-alcoholic",
       alc_beverage_daily ~ "Beverages, alcoholic"
