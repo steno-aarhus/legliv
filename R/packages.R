bb3caa68bc5d5c96b8d754baa740b9dbb9123a8a
Luke W. Johnston <lwjohnst@gmail.com>
2024-03-24 16:10:27 +0100

    build: moved `install.packages()` called into formal dependency tracking in `DESCRIPTION` file.

--- R/packages.R
+++ R/packages.R
@@ -1,13 +1,3 @@
-install.packages('dplyr')
-install.packages('tidyverse')
-install.packages('magrittr')   # For the %$% composition pipe.
-install.packages('Hmisc')      # smean.cl.normal, smean.sdl.
-install.packages('DescTools')  # MedianCI.
-install.packages('confintr')   # ci_median, ci_sd.
-install.packages('blandr')     # blandr.output.text, blandr.statistics
-install.packages('parameters') # parameters.
-install.packages('psych')      # describe
-
 library(dplyr)
 library(tidyverse)
 library(magrittr)   # For the %$% composition pipe.
