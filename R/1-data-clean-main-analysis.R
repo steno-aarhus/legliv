2396d6db800b4202e239aa69b3250eab3fb14c7a
Luke W. Johnston <lwjohnst@gmail.com>
2024-03-24 16:43:42 +0100

    refactor: avoid using outside objects in functions, structure functions to chain together

--- R/1-data-clean-main-analysis.R
+++ R/1-data-clean-main-analysis.R
@@ -61,85 +61,74 @@ cancer_longer_subset <- function(data) {
 }
 
 icd10_hcc <- function(data) {
-  icd10_hcc <- icd10_subset %>%
+  data %>%
     mutate(
       icd10_hcc_date = ifelse(str_detect(p41270var, "C22.0"),
         as.character(c_across(starts_with("p41280"))),
         NA
       ),
       icd10_hcc_date = as.Date(icd10_hcc_date, format = "%Y-%m-%d")
-    )
-  first_non_na_hcc <- icd10_hcc %>%
+    ) |>
     filter(!is.na(icd10_hcc_date)) %>%
     group_by(id) %>%
     slice(1) %>%
-    ungroup()
-  data <- data %>%
-    left_join(first_non_na_hcc %>% select(id, icd10_hcc_date), by = "id")
-  return(data)
+    ungroup() |>
+    select(id, icd10_hcc_date), by = "id")
 }
-data <- icd10_hcc(data)
 
 icd10_icc <- function(data) {
-  icd10_icc <- icd10_subset %>%
+  data %>%
     mutate(
       icd10_icc_date = ifelse(str_detect(p41270var, "C22.1"),
         as.character(c_across(starts_with("p41280"))),
         NA
       ),
       icd10_icc_date = as.Date(icd10_icc_date, format = "%Y-%m-%d")
-    )
-  first_non_na_icc <- icd10_icc %>%
+    ) |>
     filter(!is.na(icd10_icc_date)) %>%
     group_by(id) %>%
     slice(1) %>%
-    ungroup()
-  data <- data %>%
-    left_join(first_non_na_icc %>% select(id, icd10_icc_date), by = "id")
-  return(data)
+    ungroup() |>
+    select(id, icd10_icc_date)
 }
-data <- icd10_icc(data)
 
 cancer_hcc <- function(data) {
-  cancer_hcc <- cancer_subset %>%
+  data %>%
     mutate(
       cancer_hcc_date = ifelse(str_detect(p40006, "C22.0"),
         as.character(c_across(starts_with("p40005"))),
         NA
       ),
       cancer_hcc_date = as.Date(cancer_hcc_date, format = "%Y-%m-%d")
-    )
-  first_non_na_hcc_c <- cancer_hcc %>%
+    ) |>
     filter(!is.na(cancer_hcc_date)) %>%
     group_by(id) %>%
     slice(1) %>%
-    ungroup()
-  data <- data %>%
-    left_join(first_non_na_hcc_c %>% select(id, cancer_hcc_date), by = "id")
-  return(data)
+    ungroup() |>
+    select(id, cancer_hcc_date)
 }
-data <- cancer_hcc(data)
 
 cancer_icc <- function(data) {
-  cancer_icc <- cancer_subset %>%
+  data %>%
     mutate(
       cancer_icc_date = ifelse(str_detect(p40006, "C22.1"),
         as.character(c_across(starts_with("p40005"))),
         NA
       ),
       cancer_icc_date = as.Date(cancer_icc_date, format = "%Y-%m-%d")
-    )
-  first_non_na_icc_c <- cancer_icc %>%
+    ) |>
     filter(!is.na(cancer_icc_date)) %>%
     group_by(id) %>%
     slice(1) %>%
-    ungroup()
-  data <- data %>%
-    left_join(first_non_na_icc_c %>% select(id, cancer_icc_date), by = "id")
-  return(data)
+    ungroup() |>
+    select(id, cancer_icc_date)
 }
-data <- cancer_icc(data)
 
+data <- data |>
+  left_join(icd10_hcc(icd10_subset), by = "id") |>
+  left_join(icd10_icc(icd10_subset), by = "id") |>
+  left_join(cancer_hcc(cancer_subset), by = "id") |>
+  left_join(cancer_icc(cancer_subset), by = "id")
 
 # Define baseline date ----------------------------------------------------
 
