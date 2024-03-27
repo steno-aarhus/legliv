e4ae0c42a16797f518ad37533e6d3ede134e5a26
niels bock <nb@ph.au.dk>
2024-03-27 12:47:32 +0000

    added code to make imputations.

--- R/1-data-clean-main-analysis.R
+++ R/1-data-clean-main-analysis.R
@@ -155,16 +155,9 @@ data <- opcs3_cystectomy(data)
 data <- data |>
   mutate(
     diabetes = if_else(diabetes_ins_non == "No" & diabetes_ins == "No" & diabetes_icd9 == "No", "No", "Yes"),
-    cholelith = if_else(cholelith_icd10 == "No" & cholelith_icd9 == "No", "No", "Yes"),
-    cystectomy = if_else(cystectomy_opcs4 == "No" & cystectomy_opcs3 == "No", "No", "Yes"),
     nafld = if_else(nafl_icd10 == "No" & nash_icd10 == "No", "No", "Yes")
   )
 
-data <- data %>%
-  mutate(
-    gall_disease = if_else(cholelith == "No" & cystectomy == "No", "No", "Yes"),
-    high_alcohol = if_else(sex == "Male" & alcohol_daily < 56 | sex == "Female" & alcohol_daily < 42, "No", "Yes")
-  )
 
 # Removing liver cancers before baseline date -----------------------------
 remove_liver_before <- function(data) {
@@ -185,3 +178,53 @@ data <- remove_liver_before(data)
 
 data_liver <- data %>%
   filter(status == "Liver cancer")
+
+data <- data %>%
+  mutate(
+    cholelith = if_else(cholelith_icd10 == "No" & cholelith_icd9 == "No", "No", "Yes"),
+    cystectomy = if_else(cystectomy_opcs4 == "No" & cystectomy_opcs3 == "No", "No", "Yes"),
+    gall_disease = if_else(cholelith == "No" & cystectomy == "No", "No", "Yes"),
+    high_alcohol = if_else(sex == "Male" & alcohol_daily < 56 | sex == "Female" & alcohol_daily < 42, "No", "Yes")
+  )
+
+data <- data %>%
+  select(id, sex, tdi, bmi, spouse, wc, exercise, education, smoking, trigly, glucose, hdl,
+         med_men, med_women, gall_disease,
+         legume_daily_15, red_meat_daily_15, proc_meat_daily_15,
+         other_foods_daily, total_weight_food_daily, alcohol_daily,
+         status, study_time, status_age, age_at_baseline)
+
+# Perform imputation with mice
+imputed_data <- mice(data, m = 5, maxit = 5, defaultMethod = c("pmm", "logreg", "polyreg", "polr"))
+
+data <- complete(imputed_data)
+
+data %>% describe()
+
+data <- data %>%
+  mutate(
+    high_trigly = if_else(trigly >= 1.7, "Yes", "No"),
+    low_hdl = if_else(hdl <= 1.036 & sex == "Male" | hdl <= 1.295 & sex == "Female", "Yes", "No"),
+    chol_med_men = ifelse(grepl("Cholesterol lowering medication", med_men), "Yes", "No"),
+    chol_med_women = ifelse(grepl("Cholesterol lowering medication", med_women), "Yes", "No"),
+    chol_med = if_else(chol_med_men == "Yes" | chol_med_women == "Yes", "Yes", "No"),
+    low_hdl_chol_med = if_else(low_hdl == "No" & chol_med == "No", "No", "Yes"),
+    bp_med_men = ifelse(grepl("Blood pressure medication", med_men), "Yes", "No"),
+    bp_med_women = ifelse(grepl("Blood pressure medication", med_women), "Yes", "No"),
+    bp_med = if_else(bp_med_men == "Yes" | bp_med_women == "Yes", "Yes", "No"),
+    high_wc = if_else(sex == "Male" & wc >= 102 | sex == "Female" & wc >= 88, "Yes", "No"),
+    high_bmi = if_else(bmi >= 30, "Yes", "No"),
+    high_bmi_wc = if_else(high_wc == "No" & high_bmi == "No", "No", "Yes"),
+    bs_high = if_else(glucose >= 39, "Yes", "No"),
+    ins_med_men = ifelse(grepl("Insulin", med_men), "Yes", "No"),
+    ins_med_women = ifelse(grepl("Insulin", med_women), "Yes", "No"),
+    ins_med = if_else(ins_med_men == "Yes" | ins_med_women == "Yes", "Yes", "No"),
+    high_bs = if_else(bs_high == "No" & ins_med == "No", "No", "Yes"),
+    met_synd = case_when(
+      rowSums(is.na(select(., c("high_bmi_wc", "high_trigly", "bp_med", "low_hdl_chol_med", "high_bs")))) > 0 ~ NA_character_,
+      rowSums(select(., c("high_bmi_wc", "high_trigly", "bp_med", "low_hdl_chol_med", "high_bs")) == "Yes", na.rm = TRUE) >= 3 ~ "Yes",
+      TRUE ~ "No"
+    )
+  )
+
+data %>% group_by(met_synd) %>% summarise(n())
