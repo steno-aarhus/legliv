1cd2749794590322bd87014c6e835a99cee47802
niels bock <nb@ph.au.dk>
2024-04-08 09:42:39 +0000

    with new food groups. other edits to clean table content.

--- R/4-diet-table.R
+++ R/4-diet-table.R
@@ -1,86 +1,88 @@
 library(gtsummary)
-diet_all <- data %>%
+diet_all_mean <- data %>%
   select(
-    total_energy_food_daily, total_weight_food_daily, legume_daily, total_meat_daily,
-    red_meat_daily, proc_meat_daily, poultry_daily, fish_daily, dairy_daily, egg_daily,
-    cereal_refined_daily, whole_grain_daily, veggie_daily, fruit_daily,
-    nut_daily, snack_daily, mixed_dish_daily, fats_daily,
-    non_alc_beverage_daily, alc_beverage_daily
+    total_energy_food_daily, total_weight_food_daily
   )
 
-diet_liver <- data %>%
+diet_all_median <- data %>%
+  select(
+    legume_daily, total_meat_daily,
+    red_meat_daily, proc_meat_daily, animal_foods, hpdi, updi, alc_beverage_daily
+  )
+
+diet_liver_mean <- data %>%
   filter(status == "Liver cancer") %>%
   select(
-    total_energy_food_daily, total_weight_food_daily, legume_daily, total_meat_daily,
-    red_meat_daily, proc_meat_daily, poultry_daily, fish_daily, dairy_daily, egg_daily,
-    cereal_refined_daily, whole_grain_daily, veggie_daily, fruit_daily,
-    nut_daily, snack_daily, mixed_dish_daily, fats_daily,
-    non_alc_beverage_daily, alc_beverage_daily
+    total_energy_food_daily, total_weight_food_daily
   )
 
-diet_all_table <- diet_all %>%
+diet_liver_median <- data %>%
+  filter(status == "Liver cancer") %>%
+  select(
+    legume_daily, total_meat_daily,
+    red_meat_daily, proc_meat_daily, animal_foods, hpdi, updi, alc_beverage_daily
+  )
+
+diet_all_table_mean <- diet_all_mean %>%
   tbl_summary(
     statistic = all_continuous() ~ "{mean} ({sd})",
     label = list(
       total_energy_food_daily ~ "Total energy intake",
-      total_weight_food_daily ~ "Total food weight",
+      total_weight_food_daily ~ "Total food intake"
+    )
+  )
+
+diet_all_table_median <- diet_all_median %>%
+  tbl_summary(
+    statistic = all_continuous() ~ "{median} ({p25}, {p75})",
+    label = list(
       legume_daily ~ "Legumes",
       total_meat_daily ~ "Red and processed meat",
       red_meat_daily ~ "Red meat",
       proc_meat_daily ~ "Processed meat",
-      poultry_daily ~ "Poultry",
-      fish_daily ~ "Fish",
-      dairy_daily ~ "Dairy",
-      egg_daily ~ "Eggs",
-      cereal_refined_daily ~ "Cereals, refined",
-      whole_grain_daily ~ "Cereals, whole grains",
-      veggie_daily ~ "Vegetables",
-      fruit_daily ~ "Fruit",
-      nut_daily ~ "Nuts & seeds",
-      snack_daily ~ "Snacks",
-      mixed_dish_daily ~ "Mixed dishes",
-      fats_daily ~ "Fat & spreads",
-      non_alc_beverage_daily ~ "Beverages, non-alcoholic",
-      alc_beverage_daily ~ "Beverages, alcoholic"
+      animal_foods ~ "Other animal-based foods",
+      hpdi ~ "Healthy plant-based foods",
+      updi ~ "Unhealthy plant-based foods",
+      alc_beverage_daily ~ "Alcoholic beverages"
     )
   )
 
-diet_liver_table <- diet_liver %>%
+diet_liver_table_mean <- diet_liver_mean %>%
   tbl_summary(
     statistic = all_continuous() ~ "{mean} ({sd})",
     label = list(
       total_energy_food_daily ~ "Total energy intake",
-      total_weight_food_daily ~ "Total food weight",
+      total_weight_food_daily ~ "Total food intake"
+    )
+  )
+
+diet_liver_table_median <- diet_liver_median %>%
+  tbl_summary(
+    statistic = all_continuous() ~ "{median} ({p25}, {p75})",
+    label = list(
       legume_daily ~ "Legumes",
       total_meat_daily ~ "Red and processed meat",
       red_meat_daily ~ "Red meat",
       proc_meat_daily ~ "Processed meat",
-      poultry_daily ~ "Poultry",
-      fish_daily ~ "Fish",
-      dairy_daily ~ "Dairy",
-      egg_daily ~ "Eggs",
-      cereal_refined_daily ~ "Cereals, refined",
-      whole_grain_daily ~ "Cereals, whole grains",
-      veggie_daily ~ "Vegetables",
-      fruit_daily ~ "Fruit",
-      nut_daily ~ "Nuts & seeds",
-      snack_daily ~ "Snacks",
-      mixed_dish_daily ~ "Mixed dishes",
-      fats_daily ~ "Fat & spreads",
-      non_alc_beverage_daily ~ "Beverages, non-alcoholic",
-      alc_beverage_daily ~ "Beverages, alcoholic"
+      animal_foods ~ "Other animal-based foods",
+      hpdi ~ "Healthy plant-based foods",
+      updi ~ "Unhealthy plant-based foods",
+      alc_beverage_daily ~ "Alcoholic beverages"
     )
   )
 
-diet_combined_table <- tbl_merge(
-  tbls = list(diet_all_table, diet_liver_table),
-  tab_spanner = c("**Cohort**", "**Liver cancer**")
-) %>%
+row1 <- tbl_merge(list(diet_all_table_mean, diet_liver_table_mean), tab_spanner = c("**Cohort**", "**Liver cancer**")) %>%
   bold_labels() %>%
-  modify_caption("**Table 2. Daily dietary intake.**")
+  modify_caption("**Table 2. Daily dietary intake.**") %>%
+  modify_footnote(all_stat_cols() ~ "mean (±SD) for total energy and food intake; median (IQR) for food groups")
+row2 <- tbl_merge(list(diet_all_table_median, diet_liver_table_median)) %>%
+  bold_labels()
+diet_table <-
+  tbl_stack(list(row1, row2))
+diet_table
 
-diet_combined_table %>%
-  as_gt() %>% # convert to gt table
-  gt::gtsave( # save table as image
-    filename = "table-diet.png", path = "~/legliv/doc/Images"
-  )
+# diet_table %>%
+#   as_gt() %>% # convert to gt table
+#   gt::gtsave( # save table as image
+#     filename = "table-diet.png", path = "~/legliv/doc/Images"
+#   )
