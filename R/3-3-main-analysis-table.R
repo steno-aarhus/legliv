5a2821d1654302032405a823165feca3bf840234
niels bock <nb@ph.au.dk>
2024-04-14 17:12:18 +0000

    edits to tidy table

--- R/3-3-main-analysis-table.R
+++ R/3-3-main-analysis-table.R
@@ -1,4 +1,5 @@
 library(survival)
+library(gt)
 library(gtsummary)
 model1t <- coxph(
   Surv(time = status_age, event = status == "Liver cancer") ~
@@ -59,43 +60,42 @@ m1t <- model1t %>%
   tbl_regression(
     exponentiate = T,
     include = legume_daily_15,
-    label = legume_daily_15 ~ " ",
-  ) %>%
-  modify_caption("**Table 3. Hazard ratios for substitution of legumes for total meat, red meat and processed meat.** (N = {N})")
+    label = legume_daily_15 ~ "Legumes for total meat",
+  )
 
 m1r <- model1r %>%
   tbl_regression(
     exponentiate = T,
     include = legume_daily_15,
-    label = legume_daily_15 ~ " ",
+    label = legume_daily_15 ~ "Legumes for red meat",
   )
 
 m1p <- model1p %>%
   tbl_regression(
     exponentiate = T,
     include = legume_daily_15,
-    label = legume_daily_15 ~ " ",
+    label = legume_daily_15 ~ "Legumes for processed meat",
   )
 
 m2t <- model2t %>%
   tbl_regression(
     exponentiate = T,
     include = legume_daily_15,
-    label = legume_daily_15 ~ " ",
+    label = legume_daily_15 ~ "Legumes for total meat",
   )
 
 m2r <- model2r %>%
   tbl_regression(
     exponentiate = T,
     include = legume_daily_15,
-    label = legume_daily_15 ~ " ",
+    label = legume_daily_15 ~ "Legumes for red meat",
   )
 
 m2p <- model2p %>%
   tbl_regression(
     exponentiate = T,
     include = legume_daily_15,
-    label = legume_daily_15 ~ " ",
+    label = legume_daily_15 ~ "Legumes for processed meat",
   )
 
 row1 <- tbl_merge(list(m1t, m2t)) %>%
@@ -104,10 +104,10 @@ row2 <- tbl_merge(list(m1r, m2r))
 row3 <- tbl_merge(list(m1p, m2p))
 
 table_main <-
-  tbl_stack(list(row1, row2, row3),
-            group_header = c("Legumes for total meat", "Legumes for red meat", "Legumes for processed meat"),
-            quiet = TRUE) %>%
+  tbl_stack(list(row1, row2, row3)) %>%
+  modify_caption("**Table 3. Hazard ratios for substitution of legumes for total meat, red meat and processed meat.** (N = {N})") %>%
   modify_header(label = "**15 g/day substitution**") %>%
+  modify_footnote(update = everything() ~ NA, abbreviation = T) %>%
   as_gt() %>%
   tab_spanner(
     label = "Crude",
@@ -118,11 +118,11 @@ table_main <-
     columns = c(estimate_2, ci_2, p.value_2)
   ) %>%
   tab_footnote(
-    footnote = "123",
+    footnote = "Minimally adjusted to fit the substitution model: adjusted for age (as underlying timescale), other food groups and total food intake.",
     locations = cells_column_spanners(spanners = "Crude")
   ) %>%
   tab_footnote(
-    footnote = "456",
+    footnote = "Further adjusted for sex, educational level, Townsend Deprivation Index, living alone, physical activity, smoking, alcohol intake and waist circumference.",
     locations = cells_column_spanners(spanners = "Adjusted")
   )
 table_main
