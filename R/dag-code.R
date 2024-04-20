90cd24cbc47efdbb3335af22d62d77a31cb07f09
Niels Bock <nb@ph.au.dk>
2024-04-21 00:02:52 +0200

    Code for nice dag figure

--- R/dag-code.R
+++ R/dag-code.R
@@ -1,75 +1,89 @@
 library(tidyverse)
 library(ggdag)
+library(dagitty)
 
-legliv <- dagitty::dagitty(
-'dag {
-bb="-7.026,-4.028,6.224,4.491"
-"Intake of other foods (g)" [pos="-5.742,-1.085"]
-"Legume intake (g)" [pos="-5.691,3.519"]
-"Liver cancer" [outcome,pos="5.094,1.535"]
-"Living alone" [pos="0.921,-3.639"]
-"Metabolic syndrome" [pos="-2.327,3.117"]
-"Physical activity" [pos="2.141,-3.406"]
-"Replacing meat with legumes (g)" [exposure,pos="-5.023,1.535"]
-"Socioeconomic status" [pos="0.703,-1.850"]
-"Total food intake" [pos="-6.076,0.147"]
-"Waist circumference" [pos="-4.689,-3.457"]
-"red meat intake (g)" [pos="-6.063,2.287"]
-Adiposity [pos="-4.689,-2.070"]
-Age [pos="-1.479,-1.940"]
-Alcohol [pos="4.606,-3.341"]
-Diabetes [pos="2.924,2.468"]
-Education [pos="-0.709,-3.756"]
-Lifestyle [pos="3.361,-1.979"]
-NAFLD [pos="2.937,3.700"]
-Sex [pos="-3.315,-2.239"]
-Smoking [pos="3.451,-3.354"]
-tdi [pos="-1.967,-3.730"]
+legliv <- dagitty(
+  'dag {
+bb="-8,-5,6,5"
+"Intake of other foods (g)" [pos="4.000,0.000"]
+"Legume intake (g)" [pos="-1.000,4.000"]
+"Liver cancer" [outcome,pos="0.000,-4.000"]
+"Living alone" [pos="-3.500,0.000"]
+"Physical activity" [pos="-3.500,-1.000"]
+"Replacing meat with legumes (g)" [exposure,pos="0.000,2.000"]
+"Socioeconomic status" [pos="-6.000,1.000"]
+"Total food intake (g)" [pos="4.000,-1.000"]
+"Waist circumference" [pos="-3.500,-4.000"]
+"red meat intake (g)" [pos="1.000,4.000"]
+Adiposity [pos="-6.000,-4.000"]
+Age [pos="-3.500,4.000"]
+Alcohol [pos="-3.500,-2.000"]
+Education [pos="-3.500,2.000"]
+Lifestyle [pos="-6.000,-2.000"]
+Sex [pos="-3.500,3.000"]
+Smoking [pos="-3.500,-3.000"]
+tdi [pos="-3.500,1.000"]
 "Intake of other foods (g)" -> "Liver cancer"
 "Intake of other foods (g)" -> "Replacing meat with legumes (g)"
 "Legume intake (g)" -> "Replacing meat with legumes (g)"
-"Living alone" -> "Socioeconomic status"
-"Metabolic syndrome" -> Diabetes
-"Metabolic syndrome" -> NAFLD
-"Physical activity" -> Lifestyle
+"Living alone" -> "Liver cancer"
+"Living alone" -> "Replacing meat with legumes (g)"
+"Physical activity" -> "Liver cancer"
+"Physical activity" -> "Replacing meat with legumes (g)"
 "Replacing meat with legumes (g)" -> "Liver cancer"
-"Replacing meat with legumes (g)" -> "Metabolic syndrome"
-"Socioeconomic status" -> "Liver cancer"
-"Socioeconomic status" -> "Replacing meat with legumes (g)"
-"Total food intake" -> "Liver cancer"
-"Total food intake" -> "Replacing meat with legumes (g)"
-"Waist circumference" -> Adiposity
+"Socioeconomic status" -> "Living alone"
+"Socioeconomic status" -> Education
+"Socioeconomic status" -> tdi
+"Total food intake (g)" -> "Liver cancer"
+"Total food intake (g)" -> "Replacing meat with legumes (g)"
+"Waist circumference" -> "Liver cancer"
+"Waist circumference" -> "Replacing meat with legumes (g)"
 "red meat intake (g)" -> "Replacing meat with legumes (g)"
-Adiposity -> "Liver cancer"
-Adiposity -> "Replacing meat with legumes (g)"
+Adiposity -> "Waist circumference"
 Age -> "Liver cancer"
 Age -> "Replacing meat with legumes (g)"
-Alcohol -> Lifestyle
-Diabetes -> "Liver cancer"
-Education -> "Socioeconomic status"
-Lifestyle -> "Liver cancer"
-Lifestyle -> "Replacing meat with legumes (g)"
-NAFLD -> "Liver cancer"
+Alcohol -> "Liver cancer"
+Alcohol -> "Replacing meat with legumes (g)"
+Education -> "Liver cancer"
+Education -> "Replacing meat with legumes (g)"
+Lifestyle -> "Physical activity"
+Lifestyle -> Alcohol
+Lifestyle -> Smoking
 Sex -> "Liver cancer"
 Sex -> "Replacing meat with legumes (g)"
-Smoking -> Lifestyle
-tdi -> "Socioeconomic status"
-}'
+Smoking -> "Liver cancer"
+Smoking -> "Replacing meat with legumes (g)"
+tdi -> "Liver cancer"
+tdi -> "Replacing meat with legumes (g)"
+}
+
+'
 )
 
-legliv_tidy <- tidy_dagitty(legliv) %>% dag_adjustment_sets(exposure = "Replacing meat with legumes (g)", outcome = "Liver cancer") %>%
+legliv_tidy <- tidy_dagitty(legliv) %>%
+  dag_adjustment_sets(type = "minimal",
+                      effect = "total") %>%
   mutate(colour = case_when(
-    name == "Liver cancer" ~ "outcome",
-    name == "Replacing meat with legumes (g)" ~ "exposure",
-    name == "Legume intake (g)" | name == "red meat intake (g)" ~ "food",
-    name == "Metabolic syndrome" | name == "NAFLD" | name == "Diabetes" ~ "met",
-    name != "Liver cancer" & name != "Replacing red meat with legumes (g) ~ exposure" ~ "adjusted"
+    x == "1" | x == "-1" ~ "exposure",
+    x == "0" & y == "2" ~ "exposure",
+    x == "4" | x == "-6" | x == "-3.5" ~ "confounders",
+    x == "0" & y == "-4" ~ "liver cancer",
   ))
-legliv_tidy |>
-  ggdag(text = FALSE) +
-  geom_dag_point(aes(colour = colour)) +
-  theme_dag() +
-  theme(legend.position = "none") +
-  geom_dag_label(color="black", size = 2.5) +
-  scale_adjusted()
 
+dag <- legliv_tidy |>
+  ggdag_adjustment_set(text = FALSE,
+                       exposure = "Replacing meat with legumes (g)",
+                       outcome = "Liver cancer",
+                       shadow = TRUE,
+                       stylized = TRUE,
+                       type = "minimal",
+                       effect = "total",
+                       node_size = 12,
+                       expand_x = expansion(c(0.1, 0.1)),
+                       expand_y = expansion(c(0.1, 0.1))) +
+  geom_dag_node(aes(colour = colour)) +
+  scale_color_manual(values = c("black", "darkred", "darkgreen", "darkblue", "black")) +
+  theme_dag(base_size = 0) +
+  theme(legend.position = "none") +
+  geom_dag_label(color="black", size = 2) +
+  scale_adjusted()
