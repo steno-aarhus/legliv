a95a8a64f11a7221dde74c977700261bd3a59286
Niels Bock <nb@ph.au.dk>
2024-04-22 17:10:12 +0200

    tidied the plot

--- R/dag-code.R
+++ R/dag-code.R
@@ -5,24 +5,24 @@ library(dagitty)
 legliv <- dagitty( # Har først lavet grafen i dagitty og så kopieret koden ind her. Her er det relativt let at justere koordinaterne så det står helt skarpt.
   'dag {
 bb="-8,-5,6,5"
-"Intake of other foods (g)" [pos="4.000,0.000"]
-"Legume intake (g)" [pos="-1.000,4.000"]
-"Liver cancer" [outcome,pos="0.000,-4.000"]
+"Intake of other foods (g)" [pos="3.500,-1.000"]
+"Legume intake (g)" [pos="-1.000,3.000"]
+"Liver cancer" [outcome,pos="0.000,-3.000"]
 "Living alone" [pos="-3.500,0.000"]
 "Physical activity" [pos="-3.500,-1.000"]
-"Replacing meat with legumes (g)" [exposure,pos="0.000,2.000"]
+"Red meat intake (g)" [pos="1.000,3.000"]
+"Replacing meat with legumes (g)" [exposure,pos="0.000,1.000"]
 "Socioeconomic status" [pos="-6.000,1.000"]
-"Total food intake (g)" [pos="4.000,-1.000"]
+"Total food intake (g)" [pos="3.500,-2.000"]
 "Waist circumference" [pos="-3.500,-4.000"]
-"red meat intake (g)" [pos="1.000,4.000"]
 Adiposity [pos="-6.000,-4.000"]
-Age [pos="-3.500,4.000"]
+Age [pos="3.500,0.000"]
 Alcohol [pos="-3.500,-2.000"]
 Education [pos="-3.500,2.000"]
 Lifestyle [pos="-6.000,-2.000"]
 Sex [pos="-3.500,3.000"]
 Smoking [pos="-3.500,-3.000"]
-tdi [pos="-3.500,1.000"]
+TDI [pos="-3.500,1.000"]
 "Intake of other foods (g)" -> "Liver cancer"
 "Intake of other foods (g)" -> "Replacing meat with legumes (g)"
 "Legume intake (g)" -> "Replacing meat with legumes (g)"
@@ -30,15 +30,15 @@ tdi [pos="-3.500,1.000"]
 "Living alone" -> "Replacing meat with legumes (g)"
 "Physical activity" -> "Liver cancer"
 "Physical activity" -> "Replacing meat with legumes (g)"
+"Red meat intake (g)" -> "Replacing meat with legumes (g)"
 "Replacing meat with legumes (g)" -> "Liver cancer"
 "Socioeconomic status" -> "Living alone"
 "Socioeconomic status" -> Education
-"Socioeconomic status" -> tdi
+"Socioeconomic status" -> TDI
 "Total food intake (g)" -> "Liver cancer"
 "Total food intake (g)" -> "Replacing meat with legumes (g)"
 "Waist circumference" -> "Liver cancer"
 "Waist circumference" -> "Replacing meat with legumes (g)"
-"red meat intake (g)" -> "Replacing meat with legumes (g)"
 Adiposity -> "Waist circumference"
 Age -> "Liver cancer"
 Age -> "Replacing meat with legumes (g)"
@@ -53,10 +53,9 @@ Sex -> "Liver cancer"
 Sex -> "Replacing meat with legumes (g)"
 Smoking -> "Liver cancer"
 Smoking -> "Replacing meat with legumes (g)"
-tdi -> "Liver cancer"
-tdi -> "Replacing meat with legumes (g)"
+TDI -> "Liver cancer"
+TDI -> "Replacing meat with legumes (g)"
 }
-
 '
 )
 
@@ -65,9 +64,9 @@ legliv_tidy <- tidy_dagitty(legliv) %>%
                       effect = "total") %>% # Brug "direct" hvis du også har mediatorer og vil justere for disse.
   mutate(colour = case_when( # Her laver jeg en ny kolonne som jeg bruger til at farve nodes'ene med. Kategorierne er baseret på koordinaterne, men kan i princippet baseres på navn eller retning (brug print som nedenfor at at se kolonnerne).
     x == "1" | x == "-1" ~ "exposure",
-    x == "0" & y == "2" ~ "exposure",
-    x == "4" | x == "-6" | x == "-3.5" ~ "confounders",
-    x == "0" & y == "-4" ~ "liver cancer",
+    x == "0" & y == "1" ~ "exposure",
+    x == "3.5" | x == "-3.5" | x == "-6" ~ "confounders",
+    x == "0" & y == "-3" ~ "liver cancer",
   ))
 
 # legliv_tidy %>% print() # Tjek kolonner
@@ -82,8 +81,8 @@ dag <- legliv_tidy |>
     type = "minimal",
     effect = "total",
     node_size = 12, # bliver overruled a geom_dag_node() nedenunder, så man kan ikke både ændre størrelsen på nodes og selv definere farverne...
-    expand_x = expansion(c(0.1, 0.1)),
-    expand_y = expansion(c(0.1, 0.1)) # Udnytter hele griddet
+    expand_x = expansion(c(0.1, 0.13)),
+    expand_y = expansion(c(0.075, 0.07)) # Udnytter hele griddet
   ) +
   geom_dag_node(aes(colour = colour)) + # se under node_size. "size =" argument får nodes'ene til at se mærkelige ud.
   scale_color_manual(values = c("black", "darkred", "darkgreen", "darkblue", "black")) + # "black" er plot fill-in fordi default farvekoden er defineret ud fra "adjusted" og "unadjusted" i adjusted-kolonnen.
@@ -91,3 +90,4 @@ dag <- legliv_tidy |>
   theme(legend.position = "none") + # f.eks "right" hvis du vil se legend (kan give et overblik ift. farvningen af nodes).
   geom_dag_label(color="black", size = 2) +
   scale_adjusted() # tror ikke det gør nogen forskel om den er her eller ej, i hvert fald ikke for min DAG.
+dag
