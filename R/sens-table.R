c377be3180cd0aa3ce953447b305daa8136e8d3a
nielsbock <nb@ph.au.dk>
2024-04-29 19:52:47 +0000

    added sensitivity analysis

--- R/sens-table.R
+++ R/sens-table.R
@@ -38,21 +38,21 @@ m2t_alc <- model2t_alc %>%
   tbl_regression(
     exponentiate = T,
     include = legume_daily_15,
-    label = legume_daily_15 ~ "high alcohol consumption",
+    label = legume_daily_15 ~ " ",
   )
 
 m2r_alc <- model2r_alc %>%
   tbl_regression(
     exponentiate = T,
     include = legume_daily_15,
-    label = legume_daily_15 ~ "high alcohol consumption",
+    label = legume_daily_15 ~ " ",
   )
 
 m2p_alc <- model2p_alc %>%
   tbl_regression(
     exponentiate = T,
     include = legume_daily_15,
-    label = legume_daily_15 ~ "high alcohol consumption",
+    label = legume_daily_15 ~ " ",
   )
 
 model2t_misreporter <- coxph(
@@ -92,21 +92,21 @@ m2t_misreporter <- model2t_misreporter %>%
   tbl_regression(
     exponentiate = T,
     include = legume_daily_15,
-    label = legume_daily_15 ~ "Food intak outlier",
+    label = legume_daily_15 ~ " ",
   )
 
 m2r_misreporter <- model2r_misreporter %>%
   tbl_regression(
     exponentiate = T,
     include = legume_daily_15,
-    label = legume_daily_15 ~ "Food intak outlier",
+    label = legume_daily_15 ~ " ",
   )
 
 m2p_misreporter <- model2p_misreporter %>%
   tbl_regression(
     exponentiate = T,
     include = legume_daily_15,
-    label = legume_daily_15 ~ "Food intak outlier",
+    label = legume_daily_15 ~ " ",
   )
 
 
@@ -147,21 +147,21 @@ m2t_3_ques_comp <- model2t_3_ques_comp %>%
   tbl_regression(
     exponentiate = T,
     include = legume_daily_15,
-    label = legume_daily_15 ~ "< 3 Oxford WebQs",
+    label = legume_daily_15 ~ " ",
   )
 
 m2r_3_ques_comp <- model2r_3_ques_comp %>%
   tbl_regression(
     exponentiate = T,
     include = legume_daily_15,
-    label = legume_daily_15 ~ "< 3 Oxford WebQs",
+    label = legume_daily_15 ~ " ",
   )
 
 m2p_3_ques_comp <- model2p_3_ques_comp %>%
   tbl_regression(
     exponentiate = T,
     include = legume_daily_15,
-    label = legume_daily_15 ~ "< 3 Oxford WebQs",
+    label = legume_daily_15 ~ " ",
   )
 
 model2t_liver_disease <- coxph(
@@ -201,21 +201,75 @@ m2t_liver_disease <- model2t_liver_disease %>%
   tbl_regression(
     exponentiate = T,
     include = legume_daily_15,
-    label = legume_daily_15 ~ "Liver diseases",
+    label = legume_daily_15 ~ " ",
   )
 
 m2r_liver_disease <- model2r_liver_disease %>%
   tbl_regression(
     exponentiate = T,
     include = legume_daily_15,
-    label = legume_daily_15 ~ "Liver diseases",
+    label = legume_daily_15 ~ " ",
   )
 
 m2p_liver_disease <- model2p_liver_disease %>%
   tbl_regression(
     exponentiate = T,
     include = legume_daily_15,
-    label = legume_daily_15 ~ "Liver diseases",
+    label = legume_daily_15 ~ " ",
+  )
+
+model2t_death <- coxph(
+  Surv(time = status_age, event = status == "Liver cancer") ~
+    legume_daily_15 +
+    updi + hpdi + animal_foods + alc_beverage_daily + total_weight_food_daily +
+    sex +
+    education + tdi + spouse +
+    exercise + smoking_pack + alcohol_daily +
+    wc,
+  data = data_death
+)
+
+model2r_death <- coxph(
+  Surv(time = status_age, event = status == "Liver cancer") ~
+    legume_daily_15 + proc_meat_daily_15 +
+    updi + hpdi + animal_foods + alc_beverage_daily + total_weight_food_daily +
+    sex +
+    education + tdi + spouse +
+    exercise + smoking_pack + alcohol_daily +
+    wc,
+  data = data_death
+)
+
+model2p_death <- coxph(
+  Surv(time = status_age, event = status == "Liver cancer") ~
+    legume_daily_15 + red_meat_daily_15 +
+    updi + hpdi + animal_foods + alc_beverage_daily + total_weight_food_daily +
+    sex +
+    education + tdi + spouse +
+    exercise + smoking_pack + alcohol_daily +
+    wc,
+  data = data_death
+)
+
+m2t_death <- model2t_death %>%
+  tbl_regression(
+    exponentiate = T,
+    include = legume_daily_15,
+    label = legume_daily_15 ~ " ",
+  )
+
+m2r_death <- model2r_death %>%
+  tbl_regression(
+    exponentiate = T,
+    include = legume_daily_15,
+    label = legume_daily_15 ~ " ",
+  )
+
+m2p_death <- model2p_death %>%
+  tbl_regression(
+    exponentiate = T,
+    include = legume_daily_15,
+    label = legume_daily_15 ~ " ",
   )
 
 row1 <- tbl_merge(list(m2t_alc, m2r_alc, m2p_alc)) %>%
@@ -223,10 +277,16 @@ row1 <- tbl_merge(list(m2t_alc, m2r_alc, m2p_alc)) %>%
 row2 <- tbl_merge(list(m2t_misreporter, m2r_misreporter, m2p_misreporter))
 row3 <- tbl_merge(list(m2t_3_ques_comp, m2r_3_ques_comp, m2p_3_ques_comp))
 row4 <- tbl_merge(list(m2t_liver_disease, m2r_liver_disease, m2p_liver_disease))
+row5 <- tbl_merge(list(m2t_death, m2r_death, m2p_death))
 
 table_sens <-
-  tbl_stack(list(row1, row2, row3, row4)) %>%
-  modify_caption("**Supplementary table 3. Exclusion of participants**") %>%
+  tbl_stack(list(row1, row2, row3, row4, row5),
+            group_header = c("Exclusion of partipants with a high alcohol intake:",
+                             "Exclusion of participants with a implausible food intake:",
+                             "Inclusion criteria set to >= 3 Oxfords WebQs:",
+                             "Exclusion of participants with any liver disease before baseline:",
+                             "Liver cancer as primary cause of death counts as an event:")) %>%
+  modify_caption("**Supplementary table 3. Sensitivity analyses**") %>%
   modify_header(label = "**15 g/day substitution**") %>%
   modify_footnote(update = everything() ~ NA, abbreviation = T) %>%
   modify_table_styling(
