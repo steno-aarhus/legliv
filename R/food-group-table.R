19908fd1e4134abf62c6ee34d47c7f07a967178c
niels bock <nb@ph.au.dk>
2024-05-26 21:17:21 +0000

    Made table much simpler. gtsummary theme not working

--- R/food-group-table.R
+++ R/food-group-table.R
@@ -5,12 +5,13 @@ gtsummary::set_gtsummary_theme(my_theme)
 id <- data.frame(id = 1:25)
 
 food_group <- c(
-  "Legumes", "Red meat", "Processed meat",
-  "Animal-based foods", "Poultry", "Fish", "Dairy", "Eggs", "Sauces", "Mixed dishes", "Healthy plant-based foods",
-  "Whole grains", "Fruits", "Nuts", "Plant oils", "Beverages", "Vegetables",
-  "Unhealthy plant-based foods", "Refined cereals",
-  "Potatoes", "Fruit juice", "Mixed dishes, vegetarian", "Sweets & snacks",
-  "Sugar sweetened beverages", "Alcoholic beverages"
+  "Legumes",
+  "Red meat",
+  "Processed meat",
+  "Animal-based foods",
+  "Healthy plant-based foods",
+  "Unhealthy plant-based foods",
+  "Alcoholic beverages"
 )
 food_group_df <- data.frame(food_group)
 
@@ -22,70 +23,13 @@ includes <- c(
 
   "Sausages, bacon (with and without fat), ham, liver pate",
 
-  " ",
+  "Poultry, fish, dairy, eggs, mixed dishes, and sauces and condiments",
 
-  "Fried poultry with batter/breadcrumbs, Poultry (with/without skin)",
+  "Whole grains, fruits, nuts, plant oils, beverages (water, tea and coffee), vegetables",
 
-  "Fried fish with batter/breadcrumbs, Oily fish, including salmon, Prawns, lobster, crab, shellfish,
-  Tinned tuna, white fish, other fish",
+  "Refined cereals, potatoes, fruit juice, mixed dishes (vegetarian), sweets & snacks, and sugar sweetened beverages",
 
-  "Spreadable/lower fat butter, dairy-based very low fat spread, Spreadable normal fat butter, dairy-based normal fat spread (including cholesterol lowering spread),
-  Ice cream, milk puddings, milk-based desserts, cheesecake, Dairy-based smoothies, milk-based drinks, hot chocolate,
-  Whole milk yogurt (plain), Cheese >17.5 g fat per 100 g, including hard cheese, soft cheese, spreadable, Blue, Feta, Mozzarella, Goats, other),
-  Fat free and lower fat yogurt, plain or flavoured, Cheese <=17.5g fat per 100 g, including hard and spreadable lower fat cheese, Cottage,
-  Semi-skimmed milk >1 g fat per 100 g (cow, other), Skimmed milk <1 g fat per 100 g (cow, cholesterol lowering, powdered),
-  Whole milk >3.6 g fat per 100 g (cow, goat, sheep), Cream (cow's milk)",
-
-  "Whole eggs and processed (omelette, scotch eggs, other)",
-
-  "Mayonnaise, salad dressing, pesto, cheese sauce, white sauce, gravy, Yeast, chutney, olives, ketchup, brown sauce, tomato sauce",
-
-  "Pizza (including gluten free crust), Crisps, savoury biscuits, cheese snacks, other savoury biscuits,
-  Soups, homemade, powdered and canned, Sushi",
-
-  " ",
-
-  "Mixed, brown or seeded bread, sliced, baguette, bap, roll, Wholemeal bread, sliced, baguette, bap, roll,
-  Wholewheat biscuit cereal, Bran cereal, Porridge oats (including milk/dried fruit added),
-  Oatcrunch breakfast cereal, Muesli (with or without dried fruit), Brown and wholemeal pasta and rice",
-
-  "Apples and pears, Blackberries, strawberries, blueberries, raspberries, cherries, Grapefruit, orange, satsuma,
-  Dried fruit, prunes, Bananas, mixed fruit, grapes, mango, melon, peach, pineapple, kiwi, other, Stewed fruit, plums",
-
-  "Peanut-butter and chocolate-based spread, Unsalted peanuts and nuts, Salted peanuts and nuts",
-
-  "Olive oil, Olive oil based lower fat spread, plant-based lower fat margarine and soya-based lower fat spread (including cholesterol lowering spread),
-  Olive oil based spread, plant-based soft or hard margarine and soya-based spread (including cholesterol lowering spread)",
-
-  "Normal instant, filter, cappuccino, espresso coffee, Decaffeinated instant, filter, cappuccino, espresso coffee,
-  Black, green and other tea, Decaffeinated black, herbal tea, rooibos, Plain water, sparkling water",
-
-  "Garlic, leek, onion, Broccoli, cabbage, kale, cauliflower, spinach, sprouts, Mixed side salad, lettuce, watercress,
-  Beetroot, carrots, celery, parsnip, turnip, Fresh and tinned tomatoes,
-  Mushrooms, mixed vegetables, avocado, broad beans, green beans, butternut squash, courgettes, peppers, other,
-  Coleslaw, salad with added fat/mayonnaise, guacamole, sweetcorn",
-
-  " ",
-
-  "Chocolate biscuits, plain biscuits, sweet biscuits and cookies, Naan, garlic bread, other bread (including gluten free),
-  White bread, sliced, baguette, bap, roll, Oatcrunch breakfast cereal, Crisps, savoury biscuits, cheese snacks, other savoury biscuits,
-  White pasta, rice, couscous, gluten free pasta",
-
-  "Potatoes, sweet potatoes, boiled or baked, Potatoes and chips, fried or roasted with fat, Potatoes, mashed",
-
-  "Orange, grapefruit drink and 100% fruit juice",
-
-  "Double and single crust pies, crumble pies, Yorkshire pudding, snackpot noodles,
-  Indian samosa, pakora snacks, Quorn-based and vegetarian burgers and products",
-
-  "Table sugar, honey, jam and preserves,
-  Chocolate bar (including white, milk and dark chocolate), chocolate-covered raisins, chocolate-covered sweets,
-  Pancakes, croissant, Danish pastries, scones, fruitcakes, cakes, doughnuts, sponge puddings, other desserts, cereal bars, sweet snacks,
-  Hard and soft sweets (including sugar free)",
-
-  "Rice and oat vegetable drinks, Low calorie fizzy drinks and squash, Fizzy sugary drinks, squash, fruit smoothies",
-
-  "Beer and cider, Spirits and other alcoholic drinks, Fortified wine, Red and rose wine, White wine"
+  "Beer and cider, spirits and other alcoholic drinks, fortified wine, red and rose wine, white wine"
 )
 
 includes_df <- data.frame(includes)
@@ -93,8 +37,6 @@ includes_df <- data.frame(includes)
 df <- data.frame(food_group = food_group_df,
                  includes = includes)
 
-bold_text <- c(1, 2, 3, 4, 11, 18, 25)
-indent_text <- c(5, 6)
 food_group_table <- df %>%
   gt() %>%
   tab_header(
@@ -114,13 +56,22 @@ food_group_table <- df %>%
   tab_style(
     style = cell_text(weight = "bold"),
     locations = cells_body(
-      columns = food_group,
-      rows = bold_text
+      columns = food_group
     )
   ) %>%
   tab_options(table.width = pct(100),
-              latex.use.longtable = TRUE) %>%
+              latex.use.longtable = FALSE) %>%
   cols_width(food_group ~ pct(20),
-             includes ~ pct(80))
+             includes ~ pct(80)) %>%
+  tab_options(
+    table.font.size = gt::px(13.33),
+    data_row.padding = gt::px(1),
+    summary_row.padding = gt::px(1),
+    grand_summary_row.padding = gt::px(1),
+    footnotes.padding = gt::px(1),
+    source_notes.padding = gt::px(1),
+    row_group.padding = gt::px(1),
+    table.width = pct(100)
+  )
 
 # food_group_table %>% gtsave("doc/latex-tables/table-food-group.tex")
