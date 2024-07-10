0d832af5a36e652bbb8665b36e187ee02606502e
nielsbock <nb@ph.au.dk>
2024-07-10 13:38:27 +0000

    updated to work with gtsummary 2.0

--- R/gtsummary-theme.R
+++ R/gtsummary-theme.R
@@ -1,8 +1,8 @@
 my_theme <- function() {
   list(
     "pkgwide-str:theme_name" = "my_theme()",
-    "pkgwide-fn:pvalue_fun" = function(x) gtsummary::style_pvalue(x, digits = 2),
-    "pkgwide-fn:prependpvalue_fun" = function(x) gtsummary::style_pvalue(x, digits = 2, prepend_p = TRUE),
+    "pkgwide-fn:pvalue_fun" = label_style_pvalue(digits = 2),
+    "pkgwide-fn:prependpvalue_fun" = label_style_pvalue(digits = 2, prepend_p = TRUE),
     "pkgwide-str:ci.sep" = " to ",
     "style_number-arg:decimal.mark" = ".",
     "style_number-arg:big.mark" = ",",
@@ -12,34 +12,31 @@ my_theme <- function() {
         {
           new_header_text <-
             paste0(
-              x$table_styling$header %>% filter(.data$column == "estimate") %>% pull("label"),
+              x$table_styling$header |> dplyr::filter(.data$column == "estimate") |> dplyr::pull("label"),
               " **(**",
-              x$table_styling$header %>% filter(.data$column == "ci") %>% pull("label"),
+              x$table_styling$header |> dplyr::filter(.data$column == "conf.low") |> dplyr::pull("label"),
               "**)**"
             )
 
           # adding CI footnote to any existing abbreviation footnote, e.g. for OR, HR, etc.
           estimate_footnote <-
-            x$table_styling$footnote_abbrev %>%
-            filter(.data$column %in% "estimate") %>%
-            filter(dplyr::row_number() == dplyr::n(), !is.na(.data$footnote)) %>%
-            dplyr::pull("footnote") %>%
-            c("CI = Confidence Interval") %>%
+            x$table_styling$footnote_abbrev |>
+            dplyr::filter(.data$column %in% "estimate") |>
+            dplyr::filter(dplyr::row_number() == dplyr::n(), !is.na(.data$footnote)) |>
+            dplyr::pull("footnote") |>
+            c("CI = Confidence Interval") |>
             paste(collapse = ", ")
           x %>%
             # merge estimate and CI into one cell
-            modify_table_styling(
-              columns = "estimate",
+            modify_column_merge(
               rows = !!expr(.data$variable %in% !!x$table_body$variable &
                               !is.na(.data$estimate)),
-              cols_merge_pattern = "{estimate} ({conf.low} to {conf.high})"
-            ) %>%
-            # hide ci column
-            modify_column_hide(any_of("ci")) %>%
+              pattern = "{estimate} ({conf.low} to {conf.high})"
+            ) |>
             # update column header
-            modify_header(list(estimate = new_header_text)) %>%
+            modify_header(estimate = new_header_text) |>
             # add CI abbreviation footnote
-            modify_footnote(estimate ~ estimate_footnote, abbreviation = TRUE)
+            modify_footnote(estimate = estimate_footnote, abbreviation = TRUE)
         },
         error = function(e) x
       )
@@ -50,35 +47,32 @@ my_theme <- function() {
         {
           new_header_text <-
             paste0(
-              x$table_styling$header %>% filter(.data$column == "estimate") %>% pull("label"),
+              x$table_styling$header|> dplyr::filter(.data$column == "estimate") |> dplyr::pull("label"),
               " **(", style_number(x$inputs$conf.level, scale = 100), "% CI)**"
             )
 
           # adding CI footnote to any existing abbreviation footnote, e.g. for OR, HR, etc.
           estimate_footnote <-
-            x$table_styling$footnote_abbrev %>%
-            filter(.data$column %in% "estimate") %>%
-            filter(dplyr::row_number() == dplyr::n(), !is.na(.data$footnote)) %>%
-            dplyr::pull("footnote") %>%
-            c("CI = Confidence Interval") %>%
+            x$table_styling$footnote_abbrev |>
+            dplyr::filter(.data$column %in% "estimate") |>
+            dplyr::filter(dplyr::row_number() == dplyr::n(), !is.na(.data$footnote)) |>
+            dplyr::pull("footnote") |>
+            c("CI = Confidence Interval") |>
             paste(collapse = ", ")
           x %>%
             # merge estimate and CI into one cell
-            modify_table_styling(
-              columns = "estimate",
+            modify_column_merge(
               rows = !!expr(.data$variable %in% !!x$table_body$variable &
                               !is.na(.data$estimate) &
                               !.data$reference_row %in% TRUE),
-              cols_merge_pattern = "{estimate} ({conf.low}-{conf.high})"
-            ) %>%
-            # hide ci column
-            modify_column_hide(any_of("ci")) %>%
+              pattern = "{estimate} ({conf.low}-{conf.high})"
+            ) |>
             # hide p-value column
             modify_column_hide(any_of("p.value")) %>%
             # update column header
-            modify_header(list(estimate = new_header_text)) %>%
+            modify_header(estimate = new_header_text) |>
             # add CI abbreviation footnote
-            modify_footnote(estimate ~ estimate_footnote, abbreviation = TRUE)
+            modify_footnote(estimate = estimate_footnote, abbreviation = TRUE)
         },
         error = function(e) x
       )
